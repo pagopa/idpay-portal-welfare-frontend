7da7c5ada850eadae463c6f95e70b71b70f5e5f4
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-07-07 10:07:31 +0200

    update base layout for all steps

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -62,15 +62,52 @@ export default {
       },
       stepTwo: {
         title: 'Destinatari',
+        form: {
+          title1: "Criteri d'ammissione",
+          subTitle1:
+            'Aggiungi uno o più criteri dal catalogo, oppure definisci dei criteri che i destinatari dell’iniziativa dovranno autodichiarare.',
+          criteria: 'Sfoglia criteri',
+          addManually: 'Aggiungi manualmente',
+          title2: 'Carica l’elenco degli ammessi',
+          subTitle2:
+            'Aggiungi i codici fiscali dei destinatari dell’iniziativa, così non dovranno effettuare alcun controllo d’ammissibilità.',
+          title3: 'Destinatari esclusi',
+          subTitle3:
+            'Se conosci già i destinatari esclusi dall’iniziativa, aggiungi i loro codici fiscali così non potranno effettuare l’adesione. Puoi effettuare modifiche o aggiunte anche in un secondo momento.',
+        },
       },
       stepThree: {
         title: 'Regole di spesa',
+        form: {
+          subTitle:
+            'Puoi definire delle regole sulla spesa. Le regole ti permettono di definire quali transazioni riconoscere ai fini dell’iniziativa e come usufruire dell’importo erogato.',
+          addNew: 'Aggiungi nuova',
+        },
       },
       stepFour: {
-        title: 'Regole di rimborso',
+        title1: 'Regole di rimborso',
+        title2: 'Informazioni aggiuntive',
+        form: {
+          radioQuestion: 'Su quale parametro viene erogato il rimborso?',
+          accumulatedAmount: 'Importo accumulato',
+          timeParameter: 'Parametro temporale',
+          selectTimeParam: 'Seleziona un parametro temporale',
+          subTitle: 'Inserire qui una descrizione sulle info aggiuntive',
+          idCodeBalance: 'Codice identificativo capitolo di bilancio (opzionale)',
+        },
       },
       stepFive: {
         title: 'Informazioni legali',
+        form: {
+          subTitle:
+            'Inserisci le URL dei documenti richiesti. Queste risorse dovranno essere raggiungibili pubblicamente durante tutta la durata dell’iniziativa. ',
+          infoPrivacy: 'Informativa Privacy',
+          termsCond: 'Termini e Condizioni',
+          rules: 'Regolamento',
+          helper: 'Hai bisogno di una sintesi delle regole?',
+          helperLink: 'Scarica la checklist',
+          tryUrl: 'Prova URL',
+        },
       },
       common: {
         buttons: {
@@ -79,6 +116,9 @@ export default {
           continue: 'Continua',
           send: 'Invia per la revisione',
           reset: 'Reset',
+        },
+        links: {
+          findOut: 'Scopri di più',
         },
       },
     },
