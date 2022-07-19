5b4b99a99dc3bc58b2ba5e4f03034c071b9ec334
cmartoneNTT <clemente.martone@nttdata.com>
2022-07-19 16:09:33 +0200

    remove comment into initiativeService

--- src/services/intitativeService.ts
+++ src/services/intitativeService.ts
@@ -5,7 +5,6 @@ import { InitiativeApi } from '../api/InitiativeApiClient';
 export const saveGeneralInfoService = (
   generalInfo: InitiativeGeneralDTO
 ): Promise<InitiativeDTO | void | undefined> =>
-  /* istanbul ignore if */
   InitiativeApi.initiativeGeneralPost(generalInfo).then((res) => res.initiativeId);
 
 export const getInitativeSummary = (): Promise<InitiativeGeneralDTO> =>
