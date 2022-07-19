41020fb88e998ff4de858a893c864308a8264260
cmartoneNTT <clemente.martone@nttdata.com>
2022-07-19 14:38:56 +0200

    remove comment in stepOneForm tsx

--- src/components/Wizard/components/StepOneForm.tsx
+++ src/components/Wizard/components/StepOneForm.tsx
@@ -141,7 +141,7 @@ const StepOneForm = ({ action, setAction, currentStep, setCurrentStep }: Props) 
       const formValuesParsed = parseValuesFormToInitiativeGeneralDTO(values);
       saveGeneralInfoService(formValuesParsed)
         .then((response) => {
-          console.log('PIPPONE', response);
+          console.log(response);
           dispatch(setGeneralInfo(values));
           setCurrentStep(currentStep + 1);
         })
