9c5cf82d79588f672b699a07d5e670461d0ceac1
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-08-04 19:59:29 +0200

    udpated stepOneForm

--- src/components/Wizard/__test__/StepOneForm.test.tsx
+++ src/components/Wizard/__test__/StepOneForm.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import { SetStateAction } from 'react';
 import { Provider } from 'react-redux';
 import { date } from 'yup';
@@ -10,123 +10,133 @@ import { createStore } from './../../../redux/store';
 
 describe('<StepOneForm />', (injectedStore?: ReturnType<typeof createStore>) => {
   const store = injectedStore ? injectedStore : createStore();
-  test('should display the first form, with validation on input data', () => {
-    render(
-      <Provider store={store}>
-        <StepOneForm
-          action={''}
-          // eslint-disable-next-line react/jsx-no-bind
-          setAction={function (value: SetStateAction<string>): void {
-            console.log(value);
-          }}
-          currentStep={0}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCurrentStep={function (value: SetStateAction<number>): void {
-            console.log(value);
-          }}
-        />
-      </Provider>
-    );
+  test('should display the first form, with validation on input data', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <StepOneForm
+            action={''}
+            // eslint-disable-next-line react/jsx-no-bind
+            setAction={function (value: SetStateAction<string>): void {
+              console.log(value);
+            }}
+            currentStep={0}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCurrentStep={function (value: SetStateAction<number>): void {
+              console.log(value);
+            }}
+          />
+        </Provider>
+      );
+    });
   });
 
-  it('call the submit event when form is submitted', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <Wizard />
-      </Provider>
-    );
+  it('call the submit event when form is submitted', async () => {
+    await act(async () => {
+      const { getByTestId } = render(
+        <Provider store={store}>
+          <Wizard />
+        </Provider>
+      );
 
-    // const onSubmit = jest.fn();
-    const submit = getByTestId('continue-action-test');
-    expect(WIZARD_ACTIONS.SUBMIT).toBe('SUBMIT');
-    fireEvent.click(submit);
+      // const onSubmit = jest.fn();
+      const submit = getByTestId('continue-action-test');
+      expect(WIZARD_ACTIONS.SUBMIT).toBe('SUBMIT');
+      fireEvent.click(submit);
+    });
   });
 
-  it('draf action makes the dispatch', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <Wizard />
-      </Provider>
-    );
+  it('draf action makes the dispatch', async () => {
+    await act(async () => {
+      const { getByTestId } = render(
+        <Provider store={store}>
+          <Wizard />
+        </Provider>
+      );
 
-    // const onSkip = jest.fn();
-    const skip = getByTestId('skip-action-test');
-    expect(WIZARD_ACTIONS.DRAFT).toBe('DRAFT');
-    fireEvent.click(skip);
-    // expect(onSkip).toHaveBeenCalled();
-  });
-
-  // eslint-disable-next-line sonarjs/no-identical-functions
-  it('Form Fields not null', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <StepOneForm
-          action={''}
-          // eslint-disable-next-line react/jsx-no-bind
-          setAction={function (value: SetStateAction<string>): void {
-            console.log(value);
-          }}
-          currentStep={0}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCurrentStep={function (value: SetStateAction<number>): void {
-            console.log(value);
-          }}
-        />
-      </Provider>
-    );
-    expect(getByTestId('beneficiary-type-test')).not.toBeNull();
-    expect(getByTestId('beneficiary-known-test')).not.toBeNull();
-    expect(getByTestId('budget-test')).not.toBeNull();
-    expect(getByTestId('beneficiary-budget-test')).not.toBeNull();
-    expect(getByTestId('start-date-test')).not.toBeNull();
-    expect(getByTestId('end-date-test')).not.toBeNull();
-    expect(getByTestId('ranking-start-date-test')).not.toBeNull();
-    expect(getByTestId('ranking-end-date-test')).not.toBeNull();
+      // const onSkip = jest.fn();
+      const skip = getByTestId('skip-action-test');
+      expect(WIZARD_ACTIONS.DRAFT).toBe('DRAFT');
+      fireEvent.click(skip);
+      // expect(onSkip).toHaveBeenCalled();
+    });
   });
 
   // eslint-disable-next-line sonarjs/no-identical-functions
-  it('BeneficiaryTypes / BeneficiaryKnowns have the correct values', () => {
-    const { getByLabelText } = render(
-      <Provider store={store}>
-        <StepOneForm
-          action={''}
-          // eslint-disable-next-line react/jsx-no-bind
-          setAction={function (value: SetStateAction<string>): void {
-            console.log(value);
-          }}
-          currentStep={0}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCurrentStep={function (value: SetStateAction<number>): void {
-            console.log(value);
-          }}
-        />
-      </Provider>
-    );
+  it('Form Fields not null', async () => {
+    await act(async () => {
+      const { getByTestId } = render(
+        <Provider store={store}>
+          <StepOneForm
+            action={''}
+            // eslint-disable-next-line react/jsx-no-bind
+            setAction={function (value: SetStateAction<string>): void {
+              console.log(value);
+            }}
+            currentStep={0}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCurrentStep={function (value: SetStateAction<number>): void {
+              console.log(value);
+            }}
+          />
+        </Provider>
+      );
+      expect(getByTestId('beneficiary-type-test')).not.toBeNull();
+      expect(getByTestId('beneficiary-known-test')).not.toBeNull();
+      expect(getByTestId('budget-test')).not.toBeNull();
+      expect(getByTestId('beneficiary-budget-test')).not.toBeNull();
+      expect(getByTestId('start-date-test')).not.toBeNull();
+      expect(getByTestId('end-date-test')).not.toBeNull();
+      expect(getByTestId('ranking-start-date-test')).not.toBeNull();
+      expect(getByTestId('ranking-end-date-test')).not.toBeNull();
+    });
+  });
 
-    /* Test of value of radio button */
+  // eslint-disable-next-line sonarjs/no-identical-functions
+  it('BeneficiaryTypes / BeneficiaryKnowns have the correct values', async () => {
+    await act(async () => {
+      const { getByLabelText } = render(
+        <Provider store={store}>
+          <StepOneForm
+            action={''}
+            // eslint-disable-next-line react/jsx-no-bind
+            setAction={function (value: SetStateAction<string>): void {
+              console.log(value);
+            }}
+            currentStep={0}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCurrentStep={function (value: SetStateAction<number>): void {
+              console.log(value);
+            }}
+          />
+        </Provider>
+      );
 
-    const beneficiaryType = getByLabelText(/components.wizard.stepOne.form.beneficiaryType/);
-    const beneficiaryType1 = getByLabelText(/components.wizard.stepOne.form.person/);
-    const beneficiaryType2 = getByLabelText(/components.wizard.stepOne.form.family/);
-    // const beneficiaryKnown = getByLabelText(/components.wizard.stepOne.form.beneficiaryKnown/);
-    const beneficiaryKnown1 = getByLabelText(
-      /components.wizard.stepOne.form.taxCodeList/
-    ) as HTMLInputElement;
-    const beneficiaryKnown2 = getByLabelText(
-      /components.wizard.stepOne.form.manualSelection/
-    ) as HTMLInputElement;
+      /* Test of value of radio button */
 
-    fireEvent.click(beneficiaryType);
-    expect(beneficiaryType1).toBeChecked();
-    expect(beneficiaryType2).toBeDisabled();
+      const beneficiaryType = getByLabelText(/components.wizard.stepOne.form.beneficiaryType/);
+      const beneficiaryType1 = getByLabelText(/components.wizard.stepOne.form.person/);
+      const beneficiaryType2 = getByLabelText(/components.wizard.stepOne.form.family/);
+      // const beneficiaryKnown = getByLabelText(/components.wizard.stepOne.form.beneficiaryKnown/);
+      const beneficiaryKnown1 = getByLabelText(
+        /components.wizard.stepOne.form.taxCodeList/
+      ) as HTMLInputElement;
+      const beneficiaryKnown2 = getByLabelText(
+        /components.wizard.stepOne.form.manualSelection/
+      ) as HTMLInputElement;
 
-    fireEvent.click(beneficiaryKnown1);
-    expect(beneficiaryKnown1.checked).toEqual(true);
-    expect(beneficiaryKnown2.checked).toEqual(false);
+      fireEvent.click(beneficiaryType);
+      expect(beneficiaryType1).toBeChecked();
+      expect(beneficiaryType2).toBeDisabled();
 
-    fireEvent.click(beneficiaryKnown2);
-    expect(beneficiaryKnown2.checked).toEqual(true);
-    expect(beneficiaryKnown1.checked).toEqual(false);
+      fireEvent.click(beneficiaryKnown1);
+      expect(beneficiaryKnown1.checked).toEqual(true);
+      expect(beneficiaryKnown2.checked).toEqual(false);
+
+      fireEvent.click(beneficiaryKnown2);
+      expect(beneficiaryKnown2.checked).toEqual(true);
+      expect(beneficiaryKnown1.checked).toEqual(false);
+    });
   });
 
   it('Total Budget / Budget per Person Test', async () => {
@@ -162,102 +172,108 @@ describe('<StepOneForm />', (injectedStore?: ReturnType<typeof createStore>) => 
 
     /* check if the field are required */
 
-    expect(budget).toBeRequired();
-    expect(beneficiaryBudget).toBeRequired();
-
-    /* Validation */
-
     fireEvent.change(budget, { target: { value: '1000' } });
-    expect(hasInputValueTot(budget, '1000')).toBe(true);
+
+    await act(async () => {
+      expect(hasInputValueTot(budget, '1000')).toBe(true);
+    });
 
     fireEvent.change(beneficiaryBudget, { target: { value: '100' } });
     expect(hasInputValuePerPerson(beneficiaryBudget, '100')).toBe(true);
+
+    expect(budget).toBeRequired();
+    expect(beneficiaryBudget).toBeRequired();
+    /* Validation */
   });
 
-  it('Date Join / Spend Test', () => {
-    const { getByLabelText } = render(
-      <Provider store={store}>
-        <StepOneForm
-          action={''}
-          // eslint-disable-next-line react/jsx-no-bind
-          setAction={function (value: SetStateAction<string>): void {
-            console.log(value);
-          }}
-          currentStep={0}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCurrentStep={function (value: SetStateAction<number>): void {
-            console.log(value);
-          }}
-        />
-      </Provider>
-    );
+  it('Date Join / Spend Test', async () => {
+    await act(async () => {
+      const { getByLabelText } = render(
+        <Provider store={store}>
+          <StepOneForm
+            action={''}
+            // eslint-disable-next-line react/jsx-no-bind
+            setAction={function (value: SetStateAction<string>): void {
+              console.log(value);
+            }}
+            currentStep={0}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCurrentStep={function (value: SetStateAction<number>): void {
+              console.log(value);
+            }}
+          />
+        </Provider>
+      );
 
-    function parseDateString(_value: any, originalValue: string) {
-      return isDate(originalValue) ? originalValue : parse(originalValue, 'dd-MM-yyyy', new Date());
-    }
+      function parseDateString(_value: any, originalValue: string) {
+        return isDate(originalValue)
+          ? originalValue
+          : parse(originalValue, 'dd-MM-yyyy', new Date());
+      }
 
-    function isValidDate(date: any): date is Date {
-      return date instanceof Date && !isNaN(date.getTime());
-    }
+      function isValidDate(date: any): date is Date {
+        return date instanceof Date && !isNaN(date.getTime());
+      }
 
-    const rankingStartDate = getByLabelText(/components.wizard.stepOne.form.startDate/);
-    const rankingEndDate = getByLabelText(/components.wizard.stepOne.form.endDate/);
-    const startDate = getByLabelText(/components.wizard.stepOne.form.rankingStartDate/);
-    const endDate = getByLabelText(/components.wizard.stepOne.form.rankingEndDate/);
+      const rankingStartDate = getByLabelText(/components.wizard.stepOne.form.startDate/);
+      const rankingEndDate = getByLabelText(/components.wizard.stepOne.form.endDate/);
+      const startDate = getByLabelText(/components.wizard.stepOne.form.rankingStartDate/);
+      const endDate = getByLabelText(/components.wizard.stepOne.form.rankingEndDate/);
 
-    const d = date().transform(parseDateString);
-    /* check if the fields are required */
+      const d = date().transform(parseDateString);
+      /* check if the fields are required */
 
-    expect(startDate).toBeRequired();
-    expect(endDate).toBeRequired();
+      expect(startDate).toBeRequired();
+      expect(endDate).toBeRequired();
 
-    /* Checking if invalid cast return invalid date */
+      /* Checking if invalid cast return invalid date */
 
-    expect(isValidDate(d.cast(null, { assert: false }))).toBe(false);
-    expect(isValidDate(d.cast('', { assert: false }))).toBe(false);
+      expect(isValidDate(d.cast(null, { assert: false }))).toBe(false);
+      expect(isValidDate(d.cast('', { assert: false }))).toBe(false);
 
-    /* Casting */
+      /* Casting */
 
-    expect(d.cast(new Date())).toBeInstanceOf(Date);
+      expect(d.cast(new Date())).toBeInstanceOf(Date);
 
-    /* join-from */
+      /* join-from */
 
-    fireEvent.click(rankingStartDate);
-    fireEvent.change(rankingStartDate, { target: { value: '19/07/2022' } });
-    expect(
-      d
-        .cast('19-07-2022')
-        ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
-    ).toBe('19/07/2022');
+      fireEvent.click(rankingStartDate);
+      fireEvent.change(rankingStartDate, { target: { value: '19/07/2022' } });
+      expect(
+        d
+          .cast('19-07-2022')
+          ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
+      ).toBe('19/07/2022');
 
-    /* join-to */
+      /* join-to */
 
-    fireEvent.mouseOver(rankingEndDate);
-    fireEvent.change(rankingEndDate, { target: { value: '20/07/2022' } });
-    expect(
-      d
-        .cast('20-07-2022')
-        ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
-    ).toBe('20/07/2022');
+      fireEvent.mouseOver(rankingEndDate);
+      fireEvent.change(rankingEndDate, { target: { value: '20/07/2022' } });
+      expect(
+        d
+          .cast('20-07-2022')
+          ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
+      ).toBe('20/07/2022');
 
-    /* spend-from */
+      /* spend-from */
 
-    fireEvent.mouseOver(startDate);
-    fireEvent.change(startDate, { target: { value: '21/07/2022' } });
-    expect(
-      d
-        .cast('21-07-2022')
-        ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
-    ).toBe('21/07/2022');
+      fireEvent.mouseOver(startDate);
+      fireEvent.change(startDate, { target: { value: '21/07/2022' } });
+      expect(
+        d
+          .cast('21-07-2022')
+          ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
+      ).toBe('21/07/2022');
 
-    /* spend-to */
+      /* spend-to */
 
-    fireEvent.mouseOver(endDate);
-    fireEvent.change(endDate, { target: { value: '22/07/2022' } });
-    expect(
-      d
-        .cast('22-07-2022')
-        ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
-    ).toBe('22/07/2022');
+      fireEvent.mouseOver(endDate);
+      fireEvent.change(endDate, { target: { value: '22/07/2022' } });
+      expect(
+        d
+          .cast('22-07-2022')
+          ?.toLocaleDateString('it-IT', { day: '2-digit', month: '2-digit', year: 'numeric' })
+      ).toBe('22/07/2022');
+    });
   });
 });
--- src/components/Wizard/components/StepTwo/__test__/AdmissionCriteria.test.tsx
+++ src/components/Wizard/components/StepTwo/__test__/AdmissionCriteria.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import { SetStateAction } from 'react';
+import { act } from 'react-dom/test-utils';
 import { Provider } from 'react-redux';
 import { AvailableCriteria } from '../../../../../model/AdmissionCriteria';
 import { ManualCriteriaItem } from '../../../../../model/Initiative';
@@ -12,140 +13,151 @@ import ManualCriteria from '../ManualCriteria';
 
 describe('<StepOneForm />', (injectedStore?: ReturnType<typeof createStore>) => {
   const store = injectedStore ? injectedStore : createStore();
-  test('should display the second step, with validation on input data', () => {
-    render(
-      <Provider store={store}>
-        <AdmissionCriteria
-          action={''}
-          // eslint-disable-next-line react/jsx-no-bind
-          setAction={function (value: SetStateAction<string>): void {
-            console.log(value);
-          }}
-          currentStep={1}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCurrentStep={function (value: SetStateAction<number>): void {
-            console.log(value);
-          }}
-        />
-      </Provider>
-    );
+  test('should display the second step, with validation on input data', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <AdmissionCriteria
+            action={''}
+            // eslint-disable-next-line react/jsx-no-bind
+            setAction={function (value: SetStateAction<string>): void {
+              console.log(value);
+            }}
+            currentStep={1}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCurrentStep={function (value: SetStateAction<number>): void {
+              console.log(value);
+            }}
+          />
+        </Provider>
+      );
+    });
   });
 
-  it('call the submit event when form is submitted', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <Wizard />
-      </Provider>
-    );
+  it('call the submit event when form is submitted', async () => {
+    await act(async () => {
+      const { getByTestId } = render(
+        <Provider store={store}>
+          <Wizard />
+        </Provider>
+      );
 
-    const submit = getByTestId('continue-action-test');
-    expect(WIZARD_ACTIONS.SUBMIT).toBe('SUBMIT');
-    fireEvent.click(submit);
+      const submit = getByTestId('continue-action-test');
+      expect(WIZARD_ACTIONS.SUBMIT).toBe('SUBMIT');
+      fireEvent.click(submit);
+    });
   });
 
-  it('draf action makes the dispatch', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <Wizard />
-      </Provider>
-    );
+  it('draf action makes the dispatch', async () => {
+    await act(async () => {
+      const { getByTestId } = render(
+        <Provider store={store}>
+          <Wizard />
+        </Provider>
+      );
 
-    // const onSkip = jest.fn();
-    const skip = getByTestId('skip-action-test');
-    expect(WIZARD_ACTIONS.DRAFT).toBe('DRAFT');
-    fireEvent.click(skip);
+      // const onSkip = jest.fn();
+      const skip = getByTestId('skip-action-test');
+      expect(WIZARD_ACTIONS.DRAFT).toBe('DRAFT');
+      fireEvent.click(skip);
+    });
   });
 
-  it('Test onClick of "Sfoglia Criteri" to open the modal must be true', () => {
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <AdmissionCriteria
-          action={''}
-          // eslint-disable-next-line react/jsx-no-bind
-          setAction={function (value: SetStateAction<string>): void {
-            console.log(value);
-          }}
-          currentStep={1}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCurrentStep={function (value: SetStateAction<number>): void {
-            console.log(value);
-          }}
-        />
-      </Provider>
-    );
+  it('Test onClick of "Sfoglia Criteri" to open the modal must be true', async () => {
+    await act(async () => {
+      // eslint-disable-next-line sonarjs/no-identical-functions
+      await act(async () => {
+        const { getByTestId } = render(
+          <Provider store={store}>
+            <AdmissionCriteria
+              action={''}
+              // eslint-disable-next-line react/jsx-no-bind
+              setAction={function (value: SetStateAction<string>): void {
+                console.log(value);
+              }}
+              currentStep={1}
+              // eslint-disable-next-line react/jsx-no-bind
+              setCurrentStep={function (value: SetStateAction<number>): void {
+                console.log(value);
+              }}
+            />
+          </Provider>
+        );
 
-    const criteria = getByTestId('criteria-button-test');
-    const addManually = getByTestId('add-manually-test');
+        const criteria = getByTestId('criteria-button-test');
+        const addManually = getByTestId('add-manually-test');
 
-    const criteriaModal = (
-      <AdmissionCriteriaModal
-        openModal={false}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCloseModal={function (event: React.MouseEvent<Element, MouseEvent>): void {
-          console.log(event);
-        }}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCriteriaAdded={function (event: React.MouseEvent<Element, MouseEvent>): void {
-          console.log(event);
-        }}
-        criteriaToRender={[]}
-        // eslint-disable-next-line react/jsx-no-bind
-        setCriteriaToRender={function (value: Array<AvailableCriteria>): void {
-          console.log(value);
-        }}
-      />
-    ) as unknown as HTMLElement;
+        const criteriaModal = (
+          <AdmissionCriteriaModal
+            openModal={false}
+            // eslint-disable-next-line react/jsx-no-bind
+            handleCloseModal={function (event: React.MouseEvent<Element, MouseEvent>): void {
+              console.log(event);
+            }}
+            // eslint-disable-next-line react/jsx-no-bind
+            handleCriteriaAdded={function (event: React.MouseEvent<Element, MouseEvent>): void {
+              console.log(event);
+            }}
+            criteriaToRender={[]}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCriteriaToRender={function (value: Array<AvailableCriteria>): void {
+              console.log(value);
+            }}
+          />
+        ) as unknown as HTMLElement;
 
-    const formData = {
-      _type: '',
-      description: '',
-      code: '1',
-      boolValue: true,
-      multiValue: ['', ''],
-    };
+        const formData = {
+          _type: '',
+          description: '',
+          code: '1',
+          boolValue: true,
+          multiValue: ['', ''],
+        };
 
-    const ManuallyAdded = (
-      <ManualCriteria
-        data={formData}
-        action={''}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCriteriaRemoved={function (event: React.MouseEvent<Element, MouseEvent>): void {
-          console.log(event);
-        }}
-        manualCriteriaToRender={[]}
-        // eslint-disable-next-line react/jsx-no-bind
-        setManualCriteriaToRender={function (
-          value: SetStateAction<Array<ManualCriteriaItem>>
-        ): void {
-          console.log(value);
-        }}
-        criteriaToSubmit={[]}
-        // eslint-disable-next-line react/jsx-no-bind
-        setCriteriaToSubmit={function (
-          value: SetStateAction<Array<{ code: string | undefined; dispatched: boolean }>>
-        ): void {
-          console.log(value);
-        }}
-      />
-    );
+        const ManuallyAdded = (
+          <ManualCriteria
+            data={formData}
+            action={''}
+            // eslint-disable-next-line react/jsx-no-bind
+            handleCriteriaRemoved={function (event: React.MouseEvent<Element, MouseEvent>): void {
+              console.log(event);
+            }}
+            manualCriteriaToRender={[]}
+            // eslint-disable-next-line react/jsx-no-bind
+            setManualCriteriaToRender={function (
+              value: SetStateAction<Array<ManualCriteriaItem>>
+            ): void {
+              console.log(value);
+            }}
+            criteriaToSubmit={[]}
+            // eslint-disable-next-line react/jsx-no-bind
+            setCriteriaToSubmit={function (
+              value: SetStateAction<Array<{ code: string | undefined; dispatched: boolean }>>
+            ): void {
+              console.log(value);
+            }}
+          />
+        );
 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(() => expect(criteriaModal).not.toBeInTheDocument());
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waitFor(() => expect(criteriaModal).not.toBeInTheDocument());
 
-    fireEvent.click(criteria);
-    expect(criteria).toBeTruthy();
+        fireEvent.click(criteria);
+        expect(criteria).toBeTruthy();
 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(() => expect(criteriaModal).toBeInTheDocument());
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waitFor(() => expect(criteriaModal).toBeInTheDocument());
 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(() => expect(ManuallyAdded).not.toBeInTheDocument());
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waitFor(() => expect(ManuallyAdded).not.toBeInTheDocument());
 
-    fireEvent.click(addManually);
-    expect(addManually).toBeTruthy();
+        fireEvent.click(addManually);
+        expect(addManually).toBeTruthy();
 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(() => expect(ManuallyAdded).toBeInTheDocument());
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        waitFor(() => expect(ManuallyAdded).toBeInTheDocument());
+      });
+    });
   });
 
   // eslint-disable-next-line sonarjs/no-identical-functions
