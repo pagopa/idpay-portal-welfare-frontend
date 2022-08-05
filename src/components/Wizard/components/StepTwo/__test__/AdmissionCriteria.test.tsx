34589e0ed72c4d7c279cb39c6ad8de9b05b46fec
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-08-05 13:21:03 +0200

    updated AdmissionCriteria.test

--- src/components/Wizard/components/StepTwo/__test__/AdmissionCriteria.test.tsx
+++ src/components/Wizard/components/StepTwo/__test__/AdmissionCriteria.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { SetStateAction } from 'react';
 import { act } from 'react-dom/test-utils';
 import { Provider } from 'react-redux';
@@ -11,6 +11,7 @@ import AdmissionCriteria from '../AdmissionCriteria';
 import AdmissionCriteriaModal from '../AdmissionCriteriaModal';
 import ManualCriteria from '../ManualCriteria';
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: (key: any) => key }),
 }));
@@ -47,10 +48,8 @@ describe('<AdmissionCriteria />', (injectedStore?: ReturnType<typeof createStore
       );
 
       const submit = getByTestId('continue-action-test');
-      await act(async () => {
-        expect(WIZARD_ACTIONS.SUBMIT).toBe('SUBMIT');
-      });
       fireEvent.click(submit);
+      expect(WIZARD_ACTIONS.SUBMIT).toBe('SUBMIT');
     });
   });
 
@@ -63,134 +62,134 @@ describe('<AdmissionCriteria />', (injectedStore?: ReturnType<typeof createStore
       );
 
       const skip = getByTestId('skip-action-test');
-      await act(async () => {
-        expect(WIZARD_ACTIONS.DRAFT).toBe('DRAFT');
-      });
-      fireEvent.click(skip);
+      // eslint-disable-next-line @typescript-eslint/await-thenable
+      await fireEvent.click(skip);
+      expect(WIZARD_ACTIONS.DRAFT).toBe('DRAFT');
     });
   });
 
   it('Test onClick of "Sfoglia Criteri" to open the modal must be true', async () => {
-    await act(async () => {
-      const { getByTestId } = render(
-        <Provider store={store}>
-          <AdmissionCriteria
-            action={''}
-            // eslint-disable-next-line react/jsx-no-bind
-            setAction={function (value: SetStateAction<string>): void {
-              console.log(value);
-            }}
-            currentStep={1}
-            // eslint-disable-next-line react/jsx-no-bind
-            setCurrentStep={function (value: SetStateAction<number>): void {
-              console.log(value);
-            }}
-          />
-        </Provider>
-      );
-
-      const criteria = getByTestId('criteria-button-test');
-      const addManually = getByTestId('add-manually-test');
-
-      const criteriaModal = (
-        <AdmissionCriteriaModal
-          openModal={false}
-          // eslint-disable-next-line react/jsx-no-bind
-          handleCloseModal={function (event: React.MouseEvent<Element, MouseEvent>): void {
-            console.log(event);
-          }}
-          // eslint-disable-next-line react/jsx-no-bind
-          handleCriteriaAdded={function (event: React.MouseEvent<Element, MouseEvent>): void {
-            console.log(event);
-          }}
-          criteriaToRender={[]}
-          // eslint-disable-next-line react/jsx-no-bind
-          setCriteriaToRender={function (value: Array<AvailableCriteria>): void {
-            console.log(value);
-          }}
-        />
-      ) as unknown as HTMLElement;
-
-      const formData = {
-        _type: '',
-        description: '',
-        code: '1',
-        boolValue: true,
-        multiValue: ['', ''],
-      };
-
-      const ManuallyAdded = (
-        <ManualCriteria
-          data={formData}
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <AdmissionCriteria
           action={''}
           // eslint-disable-next-line react/jsx-no-bind
-          handleCriteriaRemoved={function (event: React.MouseEvent<Element, MouseEvent>): void {
-            console.log(event);
-          }}
-          manualCriteriaToRender={[]}
-          // eslint-disable-next-line react/jsx-no-bind
-          setManualCriteriaToRender={function (
-            value: SetStateAction<Array<ManualCriteriaItem>>
-          ): void {
+          setAction={function (value: SetStateAction<string>): void {
             console.log(value);
           }}
-          criteriaToSubmit={[]}
+          currentStep={1}
           // eslint-disable-next-line react/jsx-no-bind
-          setCriteriaToSubmit={function (
-            value: SetStateAction<Array<{ code: string | undefined; dispatched: boolean }>>
-          ): void {
+          setCurrentStep={function (value: SetStateAction<number>): void {
             console.log(value);
           }}
         />
-      );
+      </Provider>
+    );
 
-      await act(async () => {
-        expect(criteriaModal).not.toBeInTheDocument();
-      });
+    const criteria = getByTestId('criteria-button-test');
+    const addManually = getByTestId('add-manually-test');
 
-      fireEvent.click(criteria);
-      await act(async () => {
-        expect(criteria).toBeTruthy();
-      });
+    const criteriaModal = (
+      <AdmissionCriteriaModal
+        openModal={false}
+        // eslint-disable-next-line react/jsx-no-bind
+        handleCloseModal={function (event: React.MouseEvent<Element, MouseEvent>): void {
+          console.log(event);
+        }}
+        // eslint-disable-next-line react/jsx-no-bind
+        handleCriteriaAdded={function (event: React.MouseEvent<Element, MouseEvent>): void {
+          console.log(event);
+        }}
+        criteriaToRender={[]}
+        // eslint-disable-next-line react/jsx-no-bind
+        setCriteriaToRender={function (value: Array<AvailableCriteria>): void {
+          console.log(value);
+        }}
+      />
+    ) as unknown as HTMLElement;
 
-      await act(async () => {
-        expect(criteriaModal).toBeInTheDocument();
-      });
+    const formData = {
+      _type: '',
+      description: '',
+      code: '1',
+      boolValue: true,
+      multiValue: ['', ''],
+    };
 
-      await act(async () => {
-        expect(ManuallyAdded).not.toBeInTheDocument();
-      });
+    const ManuallyAdded = (
+      <ManualCriteria
+        data={formData}
+        action={''}
+        // eslint-disable-next-line react/jsx-no-bind
+        handleCriteriaRemoved={function (event: React.MouseEvent<Element, MouseEvent>): void {
+          console.log(event);
+        }}
+        manualCriteriaToRender={[]}
+        // eslint-disable-next-line react/jsx-no-bind
+        setManualCriteriaToRender={function (
+          value: SetStateAction<Array<ManualCriteriaItem>>
+        ): void {
+          console.log(value);
+        }}
+        criteriaToSubmit={[]}
+        // eslint-disable-next-line react/jsx-no-bind
+        setCriteriaToSubmit={function (
+          value: SetStateAction<Array<{ code: string | undefined; dispatched: boolean }>>
+        ): void {
+          console.log(value);
+        }}
+      />
+    );
 
-      fireEvent.click(addManually);
-      await act(async () => {
-        expect(addManually).toBeTruthy();
-      });
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    waitFor(async () => {
+      expect(criteriaModal).not.toBeInTheDocument();
+    });
 
-      await act(async () => {
-        expect(ManuallyAdded).toBeInTheDocument();
-      });
+    // eslint-disable-next-line @typescript-eslint/await-thenable
+    await fireEvent.click(criteria);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    waitFor(async () => {
+      expect(criteria).toBeTruthy();
+    });
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    waitFor(async () => {
+      expect(criteriaModal).toBeInTheDocument();
+    });
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    waitFor(async () => {
+      expect(ManuallyAdded).not.toBeInTheDocument();
+    });
+
+    fireEvent.click(addManually);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    waitFor(async () => {
+      expect(addManually).toBeTruthy();
+    });
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    waitFor(async () => {
+      expect(ManuallyAdded).toBeInTheDocument();
     });
   });
-
-  // // eslint-disable-next-line sonarjs/no-identical-functions
-  // it('test add/remove ManualCriteria', async () => {
-  //   await act(async () => {
-  //     render(
-  //       <Provider store={store}>
-  //         <AdmissionCriteria
-  //           action={''}
-  //           // eslint-disable-next-line react/jsx-no-bind
-  //           setAction={function (value: SetStateAction<string>): void {
-  //             console.log(value);
-  //           }}
-  //           currentStep={1}
-  //           // eslint-disable-next-line react/jsx-no-bind
-  //           setCurrentStep={function (value: SetStateAction<number>): void {
-  //             console.log(value);
-  //           }}
-  //         />
-  //       </Provider>
-  //     );
-  //   });
-  // });
 });
+// // eslint-disable-next-line sonarjs/no-identical-functions
+// it('test add/remove ManualCriteria', async () => {
+//   await act(async () => {
+//     render(
+//       <Provider store={store}>
+//         <AdmissionCriteria
+//           action={''}
+//           // eslint-disable-next-line react/jsx-no-bind
+//           setAction={function (value: SetStateAction<string>): void {
+//             console.log(value);
+//           }}
+//           currentStep={1}
+//           // eslint-disable-next-line react/jsx-no-bind
+//           setCurrentStep={function (value: SetStateAction<number>): void {
+//             console.log(value);
+//           }}
+//         />
+//       </Provider>
+//     );
+//   });
+// });
