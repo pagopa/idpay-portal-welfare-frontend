b177a3e3d298cc502004a1f20a58ea1acb2c7f3e
cmartoneNTT <clemente.martone@nttdata.com>
2022-08-11 12:35:18 +0200

    add openApi for criteria in sidebar + regex in preGen + test

--- src/services/intitativeService.ts
+++ src/services/intitativeService.ts
@@ -1,9 +1,14 @@
+import * as t from 'io-ts';
 import { InitiativeDTO } from '../api/generated/initiative/InitiativeDTO';
 import { InitiativeGeneralDTO } from '../api/generated/initiative/InitiativeGeneralDTO';
 import { InitiativeInfoDTO } from '../api/generated/initiative/InitiativeInfoDTO';
 import { InitiativeBeneficiaryRuleDTO } from '../api/generated/initiative/InitiativeBeneficiaryRuleDTO';
 import { InitiativeApi } from '../api/InitiativeApiClient';
 import { InitiativeSummaryArrayDTO } from '../api/generated/initiative/InitiativeSummaryArrayDTO';
+import { InitiativeRewardAndTrxRulesDTORewardRule } from '../api/generated/initiative/InitiativeRewardAndTrxRulesDTO';
+import { RewardGroupDTO } from '../api/generated/initiative/RewardGroupDTO';
+import { decode } from '../utils/io-utils';
+import { RewardValueDTO } from '../api/generated/initiative/RewardValueDTO';
 
 export const saveGeneralInfoService = (
   generalInfo: InitiativeInfoDTO
@@ -35,3 +40,19 @@ export const putBeneficiaryRuleDraftService = (
 
 export const putGeneralInfo = (id: string, data: InitiativeGeneralDTO): Promise<void> =>
   InitiativeApi.initiativeGeneralPut(id, data).then((res) => res);
+
+/** It will accept a {@link InitiativeRewardAndTrxRulesDTORewardRule} and it will transcode it into {@link RewardGroupDTO} or {@link RewardValueDTO} */
+export const trascodeRewardRule = (rewardRule: InitiativeRewardAndTrxRulesDTORewardRule) => {
+  if (rewardRule) {
+    // eslint-disable-next-line @typescript-eslint/dot-notation
+    switch ((rewardRule as any)['_type']) {
+      case 'rewardGroups':
+        return decode(rewardRule, RewardGroupDTO);
+      case 'rewardValue':
+        return decode(rewardRule, RewardValueDTO);
+      default:
+        throw new Error(`Unknown type: ${rewardRule}`);
+    }
+  }
+  return rewardRule;
+};
