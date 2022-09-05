f3dd7f329273bcf65ff3717500677ec5f097184f
Francesco Graziano <fgrazian@emeal.nttdata.com>
2022-09-05 11:24:45 +0200

    removed temp configuration for groups API

--- openApi/scripts/api-groups_fixPostGen.js
+++
@@ -1,8 +0,0 @@
-const regexReplace = require('regex-replace');
-
-regexReplace(
-  'readonly sort\\?: array;',
-  'readonly sort?: Array<string>;',
-  'src/api/generated/groups/requestTypes.ts',
-  { fileContentsOnly: true }
-);
