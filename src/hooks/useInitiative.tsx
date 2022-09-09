69bc330c1410f920c56b62d17c6be5445bea0418
Francesco Graziano <fgrazian@emeal.nttdata.com>
2022-09-09 11:48:18 +0200

    some improvements on useInitiative hook

--- src/hooks/useInitiative.tsx
+++ src/hooks/useInitiative.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable functional/no-let */
-/* eslint-disable complexity */
+
 import { useLocation } from 'react-router-dom';
 import { matchPath } from 'react-router';
 import useErrorDispatcher from '@pagopa/selfcare-common-frontend/hooks/useErrorDispatcher';
@@ -30,7 +30,6 @@ import {
 import {
   AdditionalInfo,
   AutomatedCriteriaItem,
-  DaysOfWeekInterval,
   GeneralInfo,
   ManualCriteriaItem,
   RefundRule,
@@ -38,8 +37,9 @@ import {
 } from '../model/Initiative';
 import { FrequencyEnum } from '../api/generated/initiative/RewardLimitsDTO';
 import { BeneficiaryTypeEnum } from '../utils/constants';
-import { DayConfig } from '../api/generated/initiative/DayConfig';
 import { InitiativeRefundRuleDTO } from '../api/generated/initiative/InitiativeRefundRuleDTO';
+import { InitiativeDTO } from '../api/generated/initiative/InitiativeDTO';
+import { AppDispatch } from '../redux/store';
 
 interface MatchParams {
   id: string;
@@ -57,7 +57,7 @@ export const useInitiative = () => {
     exact: true,
     strict: false,
   });
-  // eslint-disable-next-line sonarjs/cognitive-complexity
+
   useEffect(() => {
     // eslint-disable-next-line no-prototype-builtins
     if (match !== null && match.params.hasOwnProperty('id')) {
@@ -73,147 +73,18 @@ export const useInitiative = () => {
           dispatch(setGeneralInfo(generalInfo));
           const additionalInfo = parseAdditionalInfo(response.additionalInfo);
           dispatch(setAdditionalInfo(additionalInfo));
-          // eslint-disable-next-line functional/no-let
-          let automatedCriteria: Array<AutomatedCriteriaItem> = [];
-          // eslint-disable-next-line functional/no-let
-          let selfDeclarationCriteria: Array<ManualCriteriaItem> = [];
-          if (
-            response.beneficiaryRule &&
-            response.beneficiaryRule.automatedCriteria &&
-            Object.keys(response.beneficiaryRule.automatedCriteria).length !== 0
-          ) {
-            automatedCriteria = [...response.beneficiaryRule.automatedCriteria];
-          }
-
-          if (
-            response.beneficiaryRule &&
-            response.beneficiaryRule.selfDeclarationCriteria &&
-            Object.keys(response.beneficiaryRule.selfDeclarationCriteria).length !== 0
-          ) {
-            const manualCriteriaFetched: Array<{
-              _type?: string;
-              description?: string;
-              value?: boolean | Array<string>;
-              code?: string;
-            }> = [...response.beneficiaryRule.selfDeclarationCriteria];
-            selfDeclarationCriteria = [...parseManualCriteria(manualCriteriaFetched)];
-          }
+          const automatedCriteria = [...parseAutomatedCriteria(response)];
           dispatch(saveAutomatedCriteria(automatedCriteria));
+          const selfDeclarationCriteria = [...parseManualCriteria(response)];
           dispatch(saveManualCriteria(selfDeclarationCriteria));
-
-          if (
-            response.rewardRule &&
-            typeof response.rewardRule !== undefined &&
-            // eslint-disable-next-line no-underscore-dangle
-            response.rewardRule._type === 'rewardValue' &&
-            // eslint-disable-next-line no-prototype-builtins
-            response.rewardRule.hasOwnProperty('rewardValue')
-          ) {
-            const rewardRule = { ...response.rewardRule } as any;
-            dispatch(saveRewardRule(rewardRule));
-          }
-          if (
-            response.trxRule &&
-            response.trxRule.threshold &&
-            typeof response.trxRule.threshold !== undefined
-          ) {
-            if (
-              response.trxRule.threshold.hasOwnProperty('from') &&
-              response.trxRule.threshold.hasOwnProperty('to')
-            ) {
-              dispatch(saveThreshold(response.trxRule.threshold));
-            } else if (
-              !response.trxRule.threshold.hasOwnProperty('from') &&
-              response.trxRule.threshold.hasOwnProperty('to')
-            ) {
-              const threshold = {
-                ...response.trxRule.threshold,
-                from: undefined,
-              };
-              dispatch(saveThreshold(threshold));
-            } else if (
-              response.trxRule.threshold.hasOwnProperty('from') &&
-              !response.trxRule.threshold.hasOwnProperty('to')
-            ) {
-              const threshold = {
-                ...response.trxRule.threshold,
-                to: undefined,
-              };
-              dispatch(saveThreshold(threshold));
-            }
-          }
-
-          if (
-            response.trxRule &&
-            response.trxRule.mccFilter &&
-            typeof response.trxRule.mccFilter !== undefined
-          ) {
-            dispatch(saveMccFilter(response.trxRule.mccFilter));
-          }
-
-          if (
-            response.trxRule &&
-            response.trxRule.trxCount &&
-            typeof response.trxRule.trxCount !== undefined
-          ) {
-            if (
-              response.trxRule.trxCount.hasOwnProperty('from') &&
-              response.trxRule.trxCount.hasOwnProperty('to')
-            ) {
-              dispatch(saveTrxCount(response.trxRule.trxCount));
-            } else if (
-              !response.trxRule.trxCount.hasOwnProperty('from') &&
-              response.trxRule.trxCount.hasOwnProperty('to')
-            ) {
-              const trxCount = {
-                ...response.trxRule.trxCount,
-                from: undefined,
-              };
-              dispatch(saveThreshold(trxCount));
-            } else if (
-              response.trxRule.trxCount.hasOwnProperty('from') &&
-              !response.trxRule.trxCount.hasOwnProperty('to')
-            ) {
-              const trxCount = {
-                ...response.trxRule.trxCount,
-                to: undefined,
-              };
-              dispatch(saveThreshold(trxCount));
-            }
-          }
-
-          if (
-            response.trxRule &&
-            response.trxRule.rewardLimits &&
-            typeof response.trxRule.rewardLimits !== undefined
-          ) {
-            const rewardLimits: Array<RewardLimit> = [];
-            if (response.trxRule.rewardLimits.length > 0) {
-              response.trxRule.rewardLimits.forEach((p) => {
-                // eslint-disable-next-line functional/immutable-data
-                rewardLimits.push({ frequency: p.frequency as string, rewardLimit: p.rewardLimit });
-              });
-            } else {
-              // eslint-disable-next-line functional/immutable-data
-              rewardLimits.push({ frequency: FrequencyEnum.DAILY, rewardLimit: undefined });
-            }
-
-            dispatch(saveRewardLimits(rewardLimits));
-          }
-
-          if (
-            response.trxRule &&
-            response.trxRule.daysOfWeek &&
-            typeof response.trxRule.daysOfWeek !== undefined
-          ) {
-            const daysOfWeek = [...response.trxRule.daysOfWeek];
-            const daysOfWeekIntervals = [...parseDaysOfWeekIntervals(daysOfWeek)];
-            dispatch(saveDaysOfWeekIntervals(daysOfWeekIntervals));
-          }
-
+          parseRewardRule(response, dispatch);
+          parseThreshold(response, dispatch);
+          parseMccFilter(response, dispatch);
+          parseTrxCount(response, dispatch);
+          parseRewardLimits(response, dispatch);
+          parseDaysOfWeekIntervals(response, dispatch);
           const refundRule = parseRefundRule(response.refundRule);
           dispatch(saveRefundRule(refundRule));
-
           // setLoading(false);
         })
         .catch((error) => {
@@ -314,85 +185,223 @@ const parseAdditionalInfo = (data: any): AdditionalInfo => {
   return dataT;
 };
 
-const parseManualCriteria = (
-  manualCriteriaFetched: Array<{
-    _type?: string;
-    description?: string;
-    value?: boolean | Array<string>;
-    code?: string;
-  }>
-): Array<ManualCriteriaItem> => {
+const parseAutomatedCriteria = (response: InitiativeDTO): Array<AutomatedCriteriaItem> => {
+  // eslint-disable-next-line functional/no-let
+  let automatedCriteria: Array<AutomatedCriteriaItem> = [];
+  if (
+    response.beneficiaryRule &&
+    response.beneficiaryRule.automatedCriteria &&
+    Object.keys(response.beneficiaryRule.automatedCriteria).length !== 0
+  ) {
+    automatedCriteria = [...response.beneficiaryRule.automatedCriteria];
+  }
+  return automatedCriteria;
+};
+
+const parseManualCriteria = (response: InitiativeDTO): Array<ManualCriteriaItem> => {
   const selfDeclarationCriteria: Array<ManualCriteriaItem> = [];
-  manualCriteriaFetched.forEach((m) => {
-    if (typeof m.value === 'boolean') {
-      // eslint-disable-next-line functional/immutable-data
-      selfDeclarationCriteria.push({
-        // eslint-disable-next-line no-underscore-dangle
-        _type: m._type,
-        boolValue: m.value,
-        multiValue: [],
-        description: m.description || '',
-        code: m.code || '',
-      });
-    } else if (Array.isArray(m.value)) {
-      // eslint-disable-next-line functional/immutable-data
-      selfDeclarationCriteria.push({
-        // eslint-disable-next-line no-underscore-dangle
-        _type: m._type,
-        boolValue: true,
-        multiValue: [...m.value],
-        description: m.description || '',
-        code: m.code || '',
-      });
-    }
-  });
+  if (
+    response.beneficiaryRule &&
+    response.beneficiaryRule.selfDeclarationCriteria &&
+    Object.keys(response.beneficiaryRule.selfDeclarationCriteria).length !== 0
+  ) {
+    const manualCriteriaFetched: Array<{
+      _type?: string;
+      description?: string;
+      value?: boolean | Array<string>;
+      code?: string;
+    }> = [...response.beneficiaryRule.selfDeclarationCriteria];
+    manualCriteriaFetched.forEach((m) => {
+      if (typeof m.value === 'boolean') {
+        // eslint-disable-next-line functional/immutable-data
+        selfDeclarationCriteria.push({
+          // eslint-disable-next-line no-underscore-dangle
+          _type: m._type,
+          boolValue: m.value,
+          multiValue: [],
+          description: m.description || '',
+          code: m.code || '',
+        });
+      } else if (Array.isArray(m.value)) {
+        // eslint-disable-next-line functional/immutable-data
+        selfDeclarationCriteria.push({
+          // eslint-disable-next-line no-underscore-dangle
+          _type: m._type,
+          boolValue: true,
+          multiValue: [...m.value],
+          description: m.description || '',
+          code: m.code || '',
+        });
+      }
+    });
+  }
   return selfDeclarationCriteria;
 };
 
-const parseDaysOfWeekIntervals = (daysOfWeek: Array<DayConfig>): Array<DaysOfWeekInterval> => {
-  const daysOfWeekIntervals: Array<{
-    daysOfWeek: string;
-    startTime: string;
-    endTime: string;
-  }> = [];
-  daysOfWeek.forEach((d) => {
-    const days: Array<string> = [];
-    const intervals: Array<{
+const parseRewardRule = (response: InitiativeDTO, dispatch: AppDispatch): void => {
+  if (
+    response.rewardRule &&
+    typeof response.rewardRule !== undefined &&
+    // eslint-disable-next-line no-underscore-dangle
+    response.rewardRule._type === 'rewardValue' &&
+    // eslint-disable-next-line no-prototype-builtins
+    response.rewardRule.hasOwnProperty('rewardValue')
+  ) {
+    const rewardRule = { ...response.rewardRule } as any;
+    dispatch(saveRewardRule(rewardRule));
+  }
+};
+
+const parseThreshold = (response: InitiativeDTO, dispatch: AppDispatch): void => {
+  if (
+    response.trxRule &&
+    response.trxRule.threshold &&
+    typeof response.trxRule.threshold !== undefined
+  ) {
+    if (
+      response.trxRule.threshold.hasOwnProperty('from') &&
+      response.trxRule.threshold.hasOwnProperty('to')
+    ) {
+      dispatch(saveThreshold(response.trxRule.threshold));
+    } else if (
+      !response.trxRule.threshold.hasOwnProperty('from') &&
+      response.trxRule.threshold.hasOwnProperty('to')
+    ) {
+      const threshold = {
+        ...response.trxRule.threshold,
+        from: undefined,
+      };
+      dispatch(saveThreshold(threshold));
+    } else if (
+      response.trxRule.threshold.hasOwnProperty('from') &&
+      !response.trxRule.threshold.hasOwnProperty('to')
+    ) {
+      const threshold = {
+        ...response.trxRule.threshold,
+        to: undefined,
+      };
+      dispatch(saveThreshold(threshold));
+    }
+  }
+};
+
+const parseMccFilter = (response: InitiativeDTO, dispatch: AppDispatch): void => {
+  if (
+    response.trxRule &&
+    response.trxRule.mccFilter &&
+    typeof response.trxRule.mccFilter !== undefined
+  ) {
+    dispatch(saveMccFilter(response.trxRule.mccFilter));
+  }
+};
+
+const parseTrxCount = (response: InitiativeDTO, dispatch: AppDispatch): void => {
+  if (
+    response.trxRule &&
+    response.trxRule.trxCount &&
+    typeof response.trxRule.trxCount !== undefined
+  ) {
+    if (
+      response.trxRule.trxCount.hasOwnProperty('from') &&
+      response.trxRule.trxCount.hasOwnProperty('to')
+    ) {
+      dispatch(saveTrxCount(response.trxRule.trxCount));
+    } else if (
+      !response.trxRule.trxCount.hasOwnProperty('from') &&
+      response.trxRule.trxCount.hasOwnProperty('to')
+    ) {
+      const trxCount = {
+        ...response.trxRule.trxCount,
+        from: undefined,
+      };
+      dispatch(saveThreshold(trxCount));
+    } else if (
+      response.trxRule.trxCount.hasOwnProperty('from') &&
+      !response.trxRule.trxCount.hasOwnProperty('to')
+    ) {
+      const trxCount = {
+        ...response.trxRule.trxCount,
+        to: undefined,
+      };
+      dispatch(saveThreshold(trxCount));
+    }
+  }
+};
+
+const parseRewardLimits = (response: InitiativeDTO, dispatch: AppDispatch): void => {
+  if (
+    response.trxRule &&
+    response.trxRule.rewardLimits &&
+    typeof response.trxRule.rewardLimits !== undefined
+  ) {
+    const rewardLimits: Array<RewardLimit> = [];
+    if (response.trxRule.rewardLimits.length > 0) {
+      response.trxRule.rewardLimits.forEach((p) => {
+        // eslint-disable-next-line functional/immutable-data
+        rewardLimits.push({ frequency: p.frequency as string, rewardLimit: p.rewardLimit });
+      });
+    } else {
+      // eslint-disable-next-line functional/immutable-data
+      rewardLimits.push({ frequency: FrequencyEnum.DAILY, rewardLimit: undefined });
+    }
+
+    dispatch(saveRewardLimits(rewardLimits));
+  }
+};
+
+const parseDaysOfWeekIntervals = (response: InitiativeDTO, dispatch: AppDispatch): void => {
+  if (
+    response.trxRule &&
+    response.trxRule.daysOfWeek &&
+    typeof response.trxRule.daysOfWeek !== undefined
+  ) {
+    const daysOfWeek = [...response.trxRule.daysOfWeek];
+    // const daysOfWeekIntervals = [...parseDaysOfWeekIntervals(daysOfWeek)];
+    const daysOfWeekIntervals: Array<{
+      daysOfWeek: string;
       startTime: string;
       endTime: string;
     }> = [];
-    if (
-      d.daysOfWeek &&
-      typeof d.daysOfWeek !== undefined &&
-      d.intervals &&
-      typeof d.intervals !== undefined
-    ) {
-      d.daysOfWeek.forEach((dd) => {
-        // eslint-disable-next-line functional/immutable-data
-        days.push(dd);
-      });
-      d.intervals.forEach((i) => {
-        const interval = {
-          startTime: i.startTime || '',
-          endTime: i.endTime || '',
-        };
-        // eslint-disable-next-line functional/immutable-data
-        intervals.push({ ...interval });
-      });
-    }
-    days.forEach((day) => {
-      intervals.forEach((intr) => {
-        const element = {
-          daysOfWeek: day,
-          startTime: intr.startTime.substring(0, 5),
-          endTime: intr.endTime.substring(0, 5),
-        };
-        // eslint-disable-next-line functional/immutable-data
-        daysOfWeekIntervals.push({ ...element });
+    daysOfWeek.forEach((d) => {
+      const days: Array<string> = [];
+      const intervals: Array<{
+        startTime: string;
+        endTime: string;
+      }> = [];
+      if (
+        d.daysOfWeek &&
+        typeof d.daysOfWeek !== undefined &&
+        d.intervals &&
+        typeof d.intervals !== undefined
+      ) {
+        d.daysOfWeek.forEach((dd) => {
+          // eslint-disable-next-line functional/immutable-data
+          days.push(dd);
+        });
+        d.intervals.forEach((i) => {
+          const interval = {
+            startTime: i.startTime || '',
+            endTime: i.endTime || '',
+          };
+          // eslint-disable-next-line functional/immutable-data
+          intervals.push({ ...interval });
+        });
+      }
+      days.forEach((day) => {
+        intervals.forEach((intr) => {
+          const element = {
+            daysOfWeek: day,
+            startTime: intr.startTime.substring(0, 5),
+            endTime: intr.endTime.substring(0, 5),
+          };
+          // eslint-disable-next-line functional/immutable-data
+          daysOfWeekIntervals.push({ ...element });
+        });
       });
     });
-  });
-  return daysOfWeekIntervals;
+
+    dispatch(saveDaysOfWeekIntervals(daysOfWeekIntervals));
+  }
 };
 
 // eslint-disable-next-line sonarjs/cognitive-complexity
