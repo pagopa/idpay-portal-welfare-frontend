dc9fe6b2369f6dfb75451f6c9753e39ebd8278d3
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-10-12 18:11:38 +0200

    updated test

--- src/hooks/__tests__/useInitiative.test.tsx
+++ src/hooks/__tests__/useInitiative.test.tsx
@@ -1,6 +1,11 @@
+import { render } from '@testing-library/react';
+import React from 'react';
 import { InitiativeApi } from '../../api/InitiativeApiClient';
 import { getInitiativeDetail } from '../../services/intitativeService';
 import { mockedInitiativeId } from '../../services/__mocks__/initiativeService';
+import { Provider } from 'react-redux';
+import { createStore } from '../../redux/store';
+import { useInitiative } from '../useInitiative';
 
 jest.mock('../../api/InitiativeApiClient');
 
@@ -13,7 +18,13 @@ jest.mock('react-redux', () => ({
   useInitiative: jest.fn(),
 }));
 
-test('test get initiative detail', async () => {
-  await getInitiativeDetail(mockedInitiativeId);
-  expect(InitiativeApi.getInitiativeById).toBeCalled();
+describe('<useInitiaitive />', (injectedStore?: ReturnType<typeof createStore>) => {
+  const store = injectedStore ? injectedStore : createStore();
+
+  test('test get initiative detail', async () => {
+    jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
+
+    await getInitiativeDetail(mockedInitiativeId);
+    expect(InitiativeApi.getInitiativeById).toBeCalledWith(mockedInitiativeId);
+  });
 });
