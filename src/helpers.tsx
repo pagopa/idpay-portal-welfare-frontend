cd0eeff9d3b52e635d308d0b06e4a0ae3d3fbf4f
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-10-25 11:38:03 +0200

    IPE-484

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -4,19 +4,61 @@ import i18n from '@pagopa/selfcare-common-frontend/locale/locale-utils';
 export const renderInitiativeStatus = (status: string | undefined) => {
   switch (status) {
     case 'DRAFT':
-      return <Chip label={i18n.t('pages.initiativeList.status.draft')} color="default" />;
+      return (
+        <Chip
+          sx={{ fontSize: '14px' }}
+          label={i18n.t('pages.initiativeList.status.draft')}
+          color="default"
+        />
+      );
     case 'IN_REVISION':
-      return <Chip label={i18n.t('pages.initiativeList.status.inRevision')} color="warning" />;
+      return (
+        <Chip
+          label={i18n.t('pages.initiativeList.status.inRevision')}
+          sx={{ fontSize: '14px' }}
+          color="warning"
+        />
+      );
     case 'TO_CHECK':
-      return <Chip label={i18n.t('pages.initiativeList.status.toCheck')} color="error" />;
+      return (
+        <Chip
+          sx={{ fontSize: '14px' }}
+          label={i18n.t('pages.initiativeList.status.toCheck')}
+          color="error"
+        />
+      );
     case 'APPROVED':
-      return <Chip label={i18n.t('pages.initiativeList.status.approved')} color="success" />;
+      return (
+        <Chip
+          sx={{ fontSize: '14px' }}
+          label={i18n.t('pages.initiativeList.status.approved')}
+          color="success"
+        />
+      );
     case 'PUBLISHED':
-      return <Chip label={i18n.t('pages.initiativeList.status.published')} color="indigo" />;
+      return (
+        <Chip
+          sx={{ fontSize: '14px' }}
+          label={i18n.t('pages.initiativeList.status.published')}
+          color="indigo"
+        />
+      );
     case 'CLOSED':
-      return <Chip label={i18n.t('pages.initiativeList.status.closed')} color="default" />;
+      return (
+        <Chip
+          sx={{ fontSize: '14px' }}
+          label={i18n.t('pages.initiativeList.status.closed')}
+          color="default"
+        />
+      );
     case 'SUSPENDED':
-      return <Chip label={i18n.t('pages.initiativeList.status.suspended')} color="error" />;
+      return (
+        <Chip
+          sx={{ fontSize: '14px' }}
+          label={i18n.t('pages.initiativeList.status.suspended')}
+          color="error"
+        />
+      );
     default:
       return null;
   }
