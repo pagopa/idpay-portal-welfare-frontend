fa059e2217287736debc160b3c47b3736a900598
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-11-24 16:15:44 +0100

    fix window.scrollTo

--- src/pages/initiativeUsers/__tests__/initiativeUsers.test.tsx
+++ src/pages/initiativeUsers/__tests__/initiativeUsers.test.tsx
@@ -1,10 +1,9 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from '../../../redux/store';
 import { mockLocationFunction } from '../../initiativeOverview/__tests__/initiativeOverview.test';
 import InitiativeUsers from '../initiativeUsers';
-// import userEvent from '@testing-library/user-event';
 
 jest.mock('react-router-dom', () => mockLocationFunction());
 
@@ -15,44 +14,32 @@ jest.mock('react-router-dom', () => ({
   }),
 }));
 
-beforeEach(() => {
-  jest.spyOn(console, 'error').mockImplementation(() => {});
-  jest.spyOn(console, 'warn').mockImplementation(() => {});
-});
-
 describe('<InitiativeUsers />', (injectedStore?: ReturnType<typeof createStore>) => {
   const store = injectedStore ? injectedStore : createStore();
 
-  const setup = () => {
-    const utils = render(
+  test('renders without crashing', () => {
+    window.scrollTo = jest.fn();
+  });
+
+  test('Test of breadcrumbs/searchUser and onChange of searchUser', async () => {
+    const { getByTestId } = render(
       <Provider store={store}>
         <InitiativeUsers />
       </Provider>
     );
-    const breadcrumbs = utils.getByTestId('breadcrumbs-test') as HTMLDivElement;
-    const searchUser = utils.getByTestId('searchUser-test') as HTMLInputElement;
-    return {
-      breadcrumbs,
-      searchUser,
-      ...utils,
-    };
-  };
+    const breadcrumbs = getByTestId('breadcrumbs-test') as HTMLDivElement;
+    const searchUser = getByTestId('searchUser-test').querySelector('input') as HTMLInputElement;
 
-  const { breadcrumbs, searchUser } = setup();
-
-  it('Test breadcrumbs', async () => {
     expect(breadcrumbs).not.toBeNull();
-    expect(breadcrumbs).not.toBeInTheDocument();
-  });
+    expect(breadcrumbs).toBeInTheDocument();
 
-  it('Test TextField searchUser', () => {
     expect(searchUser).not.toBeNull();
-    expect(searchUser).not.toBeInTheDocument();
+    expect(searchUser).toBeInTheDocument();
 
-    // userEvent.type(searchUser, '');
-    // expect(searchUser.value).toBe('');
+    fireEvent.change(searchUser, { target: { value: '' } });
+    expect(searchUser.value).toBe('');
 
-    // userEvent.type(searchUser, 'searchUser');
-    // expect(searchUser.value).toBe('searchUser');
+    fireEvent.change(searchUser, { target: { value: 'searchUser' } });
+    expect(searchUser.value).toBe('searchUser');
   });
 });
