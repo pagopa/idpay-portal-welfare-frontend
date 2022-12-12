ea32d95d78a2b3a64acb7bdeae7500e8fb17abd2
OraldoDoci <oraldo.doci@nttdat.com>
2022-12-12 10:42:17 +0100

    clean up test in general info and admin modal

--- src/components/Wizard/components/StepThree/__tests__/AdmissionCriteriaModal.test.tsx
+++ src/components/Wizard/components/StepThree/__tests__/AdmissionCriteriaModal.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { waitFor, fireEvent, act } from '@testing-library/react';
+import { waitFor, fireEvent, screen } from '@testing-library/react';
 import { AvailableCriteria } from '../../../../../model/AdmissionCriteria';
 import AdmissionCriteriaModal from '../AdmissionCriteriaModal';
 import { renderWithProviders } from '../../../../../utils/test-utils';
@@ -19,26 +19,26 @@ jest.mock('react-i18next', () => ({
 describe('<AdmissionCriteriaModal />', () => {
   const mockedCriteria: Array<AvailableCriteria> = [
     {
-      authorityLabel: '',
-      fieldLabel: '',
-      value: '',
-      value2: '',
-      code: '',
-      authority: '',
-      operator: '',
+      authorityLabel: 'auth label',
+      fieldLabel: 'field label',
+      value: 'value2',
+      value2: 'value2',
+      code: 'code1',
+      authority: 'auth1',
+      operator: 'ope1',
     },
     {
       authorityLabel: 'authLabel',
       fieldLabel: 'field',
-      value: 'value',
-      value2: 'value2',
-      code: 'code',
-      authority: 'auth',
-      operator: 'ope',
+      value: 'valueobj2',
+      value2: 'value2obj2',
+      code: 'code2',
+      authority: 'auth2',
+      operator: 'ope2',
     },
   ];
   test('Should display the Modal', async () => {
-    await waitFor(async () => {
+    await waitFor(() => {
       const { debug } = renderWithProviders(
         <AdmissionCriteriaModal
           openModal={true}
@@ -60,111 +60,34 @@ describe('<AdmissionCriteriaModal />', () => {
         />
       );
     });
-
+    /*
+    await waitFor(() => {
+      screen.debug();
+    });
+    */
     const modal = document.querySelector('[data-testid="admission-modal"') as HTMLElement;
     expect(modal).toBeInTheDocument();
-
     const fade = document.querySelector('[data-testid="admission-fade"]') as HTMLElement;
+    const searchCriteriaInput = screen.getByTestId('search-criteria-test') as HTMLInputElement;
+    const checkBoxCriteria = document.querySelectorAll(
+      '[data-testid="check-test-1"]'
+    )[0] as HTMLInputElement;
+
     expect(fade).toBeInTheDocument();
 
+    /*
+    await waitFor(() => {
+      expect(checkBoxCriteria.checked).toBe(true);
+    });
+*/
     const searchCriteria = document.querySelector(
       '[data-testid="search-criteria-test"]'
     ) as HTMLInputElement;
     fireEvent.change(searchCriteria, { target: { value: 'search critera' } });
     expect(searchCriteria).toBeInTheDocument();
-  });
 
-  // eslint-disable-next-line sonarjs/no-identical-functions
-  it('CheckBox Admission Criteria test', async () => {
-    const handleClose = jest.fn();
-    // eslint-disable-next-line sonarjs/no-identical-functions
-    const { queryByTestId } = renderWithProviders(
-      <AdmissionCriteriaModal
-        openModal={false}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCloseModal={handleClose}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCriteriaAdded={function (
-          event: React.MouseEvent<HTMLInputElement, MouseEvent>
-        ): void {
-          console.log(event);
-        }}
-        criteriaToRender={[]}
-        // eslint-disable-next-line react/jsx-no-bind
-        setCriteriaToRender={function (value: Array<AvailableCriteria>): void {
-          console.log(value);
-        }}
-      />
-    );
+    fireEvent.change(searchCriteriaInput, { target: { value: 'search criteria' } });
 
-    const checkNotSearched = queryByTestId('check-test-1') as HTMLInputElement;
-    const checkSearched = queryByTestId('check-test-2') as HTMLInputElement;
-    const closeModal = queryByTestId('close-modal-test') as HTMLInputElement;
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(async () => {
-      expect(closeModal).toBeTruthy();
-      fireEvent.click(closeModal);
-    });
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(async () => {
-      expect(handleClose).toHaveBeenCalledTimes(1);
-    });
-
-    /* test checked value */
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(async () => {
-      expect(checkNotSearched || checkSearched).toHaveAttribute('checked');
-    });
-    /* test value change */
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(async () => {
-      expect(checkNotSearched.checked).toEqual(false);
-      fireEvent.change(checkNotSearched, { target: { value: true } });
-      expect(checkNotSearched.checked).toEqual(true);
-    });
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(async () => {
-      expect(checkSearched.checked).toEqual(false);
-      fireEvent.change(checkSearched, { target: { value: true } });
-      expect(checkSearched.checked).toEqual(true);
-    });
-  });
-
-  it('test Search Criteria TextField', async () => {
-    const { queryByLabelText } = renderWithProviders(
-      <AdmissionCriteriaModal
-        openModal={false}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCloseModal={function (event: React.MouseEvent<HTMLInputElement, MouseEvent>): void {
-          console.log(event);
-        }}
-        // eslint-disable-next-line react/jsx-no-bind
-        handleCriteriaAdded={function (
-          event: React.MouseEvent<HTMLInputElement, MouseEvent>
-        ): void {
-          console.log(event);
-        }}
-        criteriaToRender={[]}
-        // eslint-disable-next-line react/jsx-no-bind
-        setCriteriaToRender={function (value: Array<AvailableCriteria>): void {
-          console.log(value);
-        }}
-      />
-    );
-
-    const searchInput = queryByLabelText(
-      /components.wizard.stepThree.chooseCriteria.modal.searchCriteria/
-    ) as HTMLInputElement;
-
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    waitFor(async () => {
-      fireEvent.change(searchInput, { target: { value: 'test' } });
-      expect(searchInput.value).toBe(true);
-    });
+    await waitFor(() => expect(searchCriteria.value).toBe('search criteria'));
   });
 });
