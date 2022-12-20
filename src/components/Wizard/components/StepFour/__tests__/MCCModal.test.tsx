8271fbb3d70d6ee6b536fb6d339e6a7703fdf7cb
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-12-20 18:23:36 +0100

    updated test for modals and StatusSnackBar

--- src/components/Wizard/components/StepFour/__tests__/MCCModal.test.tsx
+++ src/components/Wizard/components/StepFour/__tests__/MCCModal.test.tsx
@@ -16,6 +16,7 @@ describe('<MCCModal />', () => {
   const handleCloseModalMcc = jest.fn();
   const setFieldValue = jest.fn();
   const handleMccCodeCheckedUpdate = jest.fn();
+  const setMccCodesList = jest.fn();
 
   test('should render correctly the MCCModal component', async () => {
     renderWithProviders(
@@ -23,50 +24,68 @@ describe('<MCCModal />', () => {
         openModalMcc={true}
         handleCloseModalMcc={handleCloseModalMcc}
         mccCodesList={[{ code: 'code', description: 'description', checked: true }]}
-        setMccCodesList={function (_value: SetStateAction<Array<MccCodesModel>>): void {
-          //
-        }}
+        setMccCodesList={setMccCodesList}
         setFieldValue={setFieldValue}
         handleMccCodeCheckedUpdate={handleMccCodeCheckedUpdate}
       />
     );
 
     const modal = document.querySelector('[data-testid="mcc-modal"]') as HTMLElement;
-    const fade = document.querySelector('[data-testid="mcc-fade"]') as HTMLElement;
-    const closeBtn = screen.getByTestId('close-modal-test') as HTMLButtonElement;
-    const addBtn = screen.getByTestId('add-button-test') as HTMLButtonElement;
-    const serachCode = screen.getByTestId('search-code-description-test') as HTMLInputElement;
-    const selectCodes = screen.getByTestId('Select-Deselect-all-button-test') as HTMLButtonElement;
+    expect(modal).toBeInTheDocument();
 
+    const fade = document.querySelector('[data-testid="mcc-fade"]') as HTMLElement;
+    expect(fade).toBeInTheDocument();
+
+    const closeBtn = screen.getByTestId('close-modal-test') as HTMLButtonElement;
+    fireEvent.click(closeBtn);
+
+    const addBtn = screen.getByTestId('add-button-test') as HTMLButtonElement;
+    fireEvent.click(addBtn);
+
+    const selectCodes = screen.getByTestId('Select-Deselect-all-button-test') as HTMLButtonElement;
+    fireEvent.click(selectCodes);
+
+    const serachCode = screen.getByTestId('search-code-description-test') as HTMLInputElement;
     fireEvent.change(serachCode, { target: { value: '00000' } });
     expect(serachCode.value).toBe('00000');
-
-    await waitFor(async () => {
-      expect(modal).toBeInTheDocument();
-      expect(fade).toBeInTheDocument();
-      fireEvent.click(closeBtn);
-      fireEvent.click(addBtn);
-      fireEvent.click(selectCodes);
-    });
   });
 
   test('should render correctly the MCCModal component', async () => {
-    await waitFor(async () => {
-      renderWithProviders(
-        <MCCModal
-          openModalMcc={false}
-          handleCloseModalMcc={handleCloseModalMcc}
-          mccCodesList={[
-            { code: 'code', description: 'description', checked: false },
-            { code: 'code2', description: 'description2', checked: false },
-          ]}
-          setMccCodesList={function (_value: SetStateAction<Array<MccCodesModel>>): void {
-            //
-          }}
-          setFieldValue={setFieldValue}
-          handleMccCodeCheckedUpdate={handleMccCodeCheckedUpdate}
-        />
-      );
-    });
+    renderWithProviders(
+      <MCCModal
+        openModalMcc={true}
+        handleCloseModalMcc={handleCloseModalMcc}
+        mccCodesList={[
+          { code: 'code', description: 'description', checked: false },
+          { code: 'code2', description: 'description2', checked: false },
+        ]}
+        setMccCodesList={setMccCodesList}
+        setFieldValue={setFieldValue}
+        handleMccCodeCheckedUpdate={handleMccCodeCheckedUpdate}
+      />
+    );
+
+    const checkBoxMccCodes = screen.getAllByTestId('check-test-1') as HTMLInputElement[];
+    const firstCheckBox = checkBoxMccCodes[0].querySelector('input') as HTMLInputElement;
+
+    expect(firstCheckBox).toBeInTheDocument();
+    expect(firstCheckBox.checked).toEqual(false);
+    fireEvent.click(firstCheckBox);
+  });
+
+  test('should render correctly the MCCModal component', async () => {
+    renderWithProviders(
+      <MCCModal
+        openModalMcc={false}
+        handleCloseModalMcc={handleCloseModalMcc}
+        mccCodesList={[
+          { code: 'code', description: 'description', checked: false },
+          { code: 'code2', description: 'description2', checked: false },
+        ]}
+        setMccCodesList={setMccCodesList}
+        setFieldValue={setFieldValue}
+        handleMccCodeCheckedUpdate={handleMccCodeCheckedUpdate}
+      />
+    );
   });
 });
