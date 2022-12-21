acc7bba5d81bab8b31365a6c74e76a803e1549ce
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2022-12-21 11:44:15 +0100

    Updtaed initiativeList test

--- src/pages/initiativeList/__tests__/initiativeList.test.tsx
+++ src/pages/initiativeList/__tests__/initiativeList.test.tsx
@@ -40,6 +40,9 @@ describe('<InitiativeList />', (injectedHistory?: ReturnType<typeof createMemory
     const searchInitiative = screen.getByTestId('search-initiative') as HTMLInputElement;
     fireEvent.change(searchInitiative, { target: { value: 'value' } });
     expect(searchInitiative.value).toBe('value');
+
+    fireEvent.change(searchInitiative, { target: { value: '' } });
+    expect(searchInitiative.value).toBe('');
   });
 
   test('Test render InitiativeList component with review permission', async () => {
@@ -81,13 +84,17 @@ describe('<InitiativeList />', (injectedHistory?: ReturnType<typeof createMemory
       </Provider>
     );
 
-    const searchInitiative = screen.getByTestId('search-initiative-test') as HTMLInputElement;
     const createNewFullList = screen.getByTestId('create-full-onclick-test') as HTMLButtonElement;
-    const createNewEmptyList = screen.getByTestId('create-empty-onclick-test') as HTMLButtonElement;
+    fireEvent.click(createNewFullList);
 
+    const createNewEmptyList = screen.getByTestId('create-empty-onclick-test') as HTMLButtonElement;
+    fireEvent.click(createNewEmptyList);
+
+    const searchInitiative = screen.getByTestId('search-initiative-test') as HTMLInputElement;
     fireEvent.change(searchInitiative, { target: { value: 'initiative' } });
     expect(searchInitiative.value).toBe('initiative');
-    fireEvent.click(createNewFullList);
-    fireEvent.click(createNewEmptyList);
+
+    fireEvent.change(searchInitiative, { target: { value: '' } });
+    expect(searchInitiative.value).toBe('');
   });
 });
