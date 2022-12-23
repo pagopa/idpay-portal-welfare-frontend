5b12e526bf62df39f45d9ad014bd1f9561692d4f
OraldoDoci <oraldo.doci@nttdat.com>
2022-12-23 10:53:02 +0100

    test addmision criteria and changed env.test

--- src/components/Wizard/components/StepThree/__tests__/AdmissionCriteria.test.tsx
+++ src/components/Wizard/components/StepThree/__tests__/AdmissionCriteria.test.tsx
@@ -1,23 +1,29 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
-import React, { SetStateAction } from 'react';
+import { cleanup, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import { mockedInitiative } from '../../../../../model/__tests__/Initiative.test';
-import { setInitiative } from '../../../../../redux/slices/initiativeSlice';
+import {
+  saveAutomatedCriteria,
+  saveManualCriteria,
+  setGeneralInfo,
+  setInitiativeId
+} from '../../../../../redux/slices/initiativeSlice';
 import { createStore } from '../../../../../redux/store';
 import { BeneficiaryTypeEnum, WIZARD_ACTIONS } from '../../../../../utils/constants';
 import AdmissionCriteria from '../AdmissionCriteria';
 import { mapResponse } from '../helpers';
 import { mockedMapResponse } from './helpers.test';
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: (key: any) => key }),
+}));
+
 beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
   jest.spyOn(console, 'warn').mockImplementation(() => {});
 });
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: any) => key }),
-}));
+afterEach(cleanup);
 
 describe('<AdmissionCriteria />', (injectedStore?: ReturnType<typeof createStore>) => {
   const store = injectedStore ? injectedStore : createStore();
@@ -29,46 +35,119 @@ describe('<AdmissionCriteria />', (injectedStore?: ReturnType<typeof createStore
   });
 
   test('should display the second step, with validation on input data', async () => {
-    //  InitiativeApiMocked.getEligibilityCriteriaForSidebar();
-
-    const deepClonedIni = JSON.parse(JSON.stringify(mockedInitiative));
-    deepClonedIni.generalInfo = {
-      beneficiaryType: BeneficiaryTypeEnum.PF,
-      beneficiaryKnown: 'false',
-      budget: '8515',
-      beneficiaryBudget: '801',
-      rankingStartDate: new Date('2022-09-01T00:00:00.000Z'),
-      rankingEndDate: new Date('2022-09-30T00:00:00.000Z'),
-      startDate: new Date('2022-10-01T00:00:00.000Z'),
-      endDate: new Date('2023-01-31T00:00:00.000Z'),
-      introductionTextIT: 'string',
-      introductionTextEN: 'string',
-      introductionTextFR: 'string',
-      introductionTextDE: 'string',
-      introductionTextSL: 'string',
-      rankingEnabled: 'true',
-    };
-    store.dispatch(setInitiative(deepClonedIni));
+    store.dispatch(setInitiativeId('2333333'));
+    store.dispatch(
+      setGeneralInfo({
+        beneficiaryType: BeneficiaryTypeEnum.PF,
+        beneficiaryKnown: 'false',
+        budget: '8515',
+        beneficiaryBudget: '801',
+        rankingStartDate: new Date('2022-09-01T00:00:00.000Z'),
+        rankingEndDate: new Date('2022-09-30T00:00:00.000Z'),
+        startDate: new Date('2022-10-01T00:00:00.000Z'),
+        endDate: new Date('2023-01-31T00:00:00.000Z'),
+        introductionTextIT: 'string',
+        introductionTextEN: 'string',
+        introductionTextFR: 'string',
+        introductionTextDE: 'string',
+        introductionTextSL: 'string',
+        rankingEnabled: 'true',
+      })
+    );
+    store.dispatch(
+      saveAutomatedCriteria([
+        {
+          authority: 'AUTH1',
+          code: 'BIRTHDATE',
+          field: 'year',
+          operator: 'GT',
+          value: '18',
+        },
+        {
+          authority: 'AUTH1',
+          code: 'BIRTHDATE',
+          field: 'year',
+          operator: 'EQ',
+          value: '18',
+        },
+        {
+          authority: 'INPS',
+          code: 'ISEE',
+          field: 'ISEE',
+          operator: 'GT',
+          value: '40000',
+        },
+        {
+          authority: 'INPS',
+          code: 'RESIDENCE',
+          field: 'ISEE',
+          operator: 'GT',
+          value: '40000',
+        },
+      ])
+    );
+    store.dispatch(
+      saveManualCriteria([
+        {
+          _type: 'boolean',
+          description: 'string',
+          code: 'string',
+        },
+        {
+          _type: 'multi',
+          description: 'string',
+          code: 'string',
+          multiValue: [],
+        },
+      ])
+    );
     render(
       <Provider store={store}>
         <AdmissionCriteria
           action={WIZARD_ACTIONS.DRAFT}
           setAction={jest.fn()}
-          currentStep={0}
+          currentStep={3}
           setCurrentStep={jest.fn()}
           setDisabledNext={jest.fn()}
         />
       </Provider>
     );
+
+    const draft = await waitFor(() => {
+      return screen.getByText('components.wizard.common.draftSaved');
+    });
+    expect(draft).toBeInTheDocument();
+    // click the close icon on toast. id is from MUI
+    fireEvent.click(screen.getByTestId('CloseIcon'));
   });
 
   it('Test onClick of "Sfoglia Criteri" to open the modal must be true', async () => {
+    store.dispatch(setInitiativeId('2333333'));
+    store.dispatch(
+      setGeneralInfo({
+        beneficiaryType: BeneficiaryTypeEnum.PF,
+        beneficiaryKnown: 'false',
+        budget: '8515',
+        beneficiaryBudget: '801',
+        rankingStartDate: new Date('2022-09-01T00:00:00.000Z'),
+        rankingEndDate: new Date('2022-09-30T00:00:00.000Z'),
+        startDate: new Date('2022-10-01T00:00:00.000Z'),
+        endDate: new Date('2023-01-31T00:00:00.000Z'),
+        introductionTextIT: 'string',
+        introductionTextEN: 'string',
+        introductionTextFR: 'string',
+        introductionTextDE: 'string',
+        introductionTextSL: 'string',
+        rankingEnabled: 'false',
+      })
+    );
+
     const { getByTestId, queryByTestId } = render(
       <Provider store={store}>
         <AdmissionCriteria
           action={WIZARD_ACTIONS.SUBMIT}
           setAction={jest.fn()}
-          currentStep={1}
+          currentStep={3}
           setCurrentStep={jest.fn()}
           setDisabledNext={jest.fn()}
         />
