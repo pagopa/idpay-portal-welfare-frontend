7b9ffc831bfc6f3a9a4da5a6783614aa9e255571
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-01-03 14:18:33 +0100

    removed console logs

--- src/hooks/useInitiative.tsx
+++ src/hooks/useInitiative.tsx
@@ -86,7 +86,6 @@ export const useInitiative = () => {
           dispatch(setInitiativeUpdateDate(response.updateDate));
           const additionalInfo = parseAdditionalInfo(response.additionalInfo);
           dispatch(setAdditionalInfo(additionalInfo));
-          // console.log('1 ---- ', response.general?.endDate);
           const generalInfo = parseGeneralInfo(response.general);
           dispatch(setGeneralInfo(generalInfo));
           const automatedCriteria = [...parseAutomatedCriteria(response)];
@@ -224,7 +223,6 @@ export const parseGeneralInfo = (data: any): GeneralInfo => {
       dataT.startDate = data.startDate;
     }
     if (typeof data.endDate !== undefined) {
-      // console.log(data.endDate);
       // eslint-disable-next-line functional/immutable-data
       dataT.endDate = data.endDate;
     }
