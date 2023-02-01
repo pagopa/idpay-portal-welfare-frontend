f4be207ccd8e93139429f06625228147dfde3bac
OraldoDoci <oraldo.doci@nttdat.com>
2023-02-01 16:45:13 +0100

    fix test  warning translation for tos layout

--- src/components/TOSLayout/__tests__/TOSLayout.test.tsx
+++ src/components/TOSLayout/__tests__/TOSLayout.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { renderWithHistoryAndStore } from '../../../utils/test-utils';
 import TOSLayout from '../TOSLayout';
 
+beforeEach(() => {
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
+});
+
 describe('test suite for TOSLayout', () => {
   test('test render TosLayout', () => {
     renderWithHistoryAndStore(<TOSLayout />);
