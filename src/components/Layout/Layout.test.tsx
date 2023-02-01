7253dc02951bcab42f3aff869915864420bbbe95
OraldoDoci <oraldo.doci@nttdat.com>
2023-02-01 15:03:18 +0100

    test for Layout component

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
 import { renderWithHistoryAndStore } from '../../utils/test-utils';
 import Layout from '../Layout/Layout';
+
+beforeEach(() => {
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+});
 
 describe('test suite for TOSLayout', () => {
   test('test render TosLayout', () => {
