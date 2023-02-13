c383d6417970489151657e46e58e7af3f4547bdc
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-02-13 16:04:17 +0100

    Added new swagger and fix pregen for OperationDTO and OperationListDTO

--- openApi/scripts/api-initiative_fixPreGen.js
+++ openApi/scripts/api-initiative_fixPreGen.js
@@ -19,6 +19,24 @@ regexReplace(
 );
 
 regexReplace(
+  '"OperationDTO": \\{\\},',
+  '"OperationDTO":{"type": "object", "properties": { "operationId": {"type":"string"}, "operationType": {"type":"string"}, "operationDate": {"type":"string"}, "amount": {"type":"number"}, "accrued": {"type":"number"}, "circuitType": {"type":"string"}, "idTrxIssuer": {"type":"string"}, "idTrxAcquirer":{"type":"string"}, "brandLogo": {"type":"string"}, "maskedPan": {"type":"string"}, "channel": {"type":"string"}, "iban": {"type":"string"}}},',
+  'openApi/generated/initiative-swagger20.json',
+  {
+    fileContentsOnly: true,
+  }
+);
+
+regexReplace(
+  '"OperationListDTO": \\{',
+  '"OperationListDTO":{"type": "array", "items": {"$ref": "#/definitions/OperationDTO"},',
+  'openApi/generated/initiative-swagger20.json',
+  {
+    fileContentsOnly: true,
+  }
+);
+
+regexReplace(
   '"operationId": "returns-fixed-automated-criteria",',
   '"operationId":"returnsFixedAutomatedCriteria",',
   'openApi/generated/initiative-swagger20.json',
