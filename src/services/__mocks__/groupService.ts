6c20a8d33959edebd7efa2aef27126163dc78b46
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2023-02-24 13:22:50 +0100

    updated groupServices mock and add missing fileds from confrimPublishModal mock

--- src/services/__mocks__/groupService.ts
+++ src/services/__mocks__/groupService.ts
@@ -4,6 +4,21 @@ export const mockedInitiativeId = '62e29002aac2e94cfa3763dd';
 
 export const mockedFile = new File([''], 'filename', { type: 'text/html' });
 
+export const mockedBeneficiaryStatusAndDetails = {
+  beneficiariesReached: 0,
+  errorMessage: 'errorMessage',
+  fileName: 'fileName',
+  fileUploadingDateTime: new Date('2018-10-13T00:00:00.000Z'),
+  status: 'APPROVED',
+};
+
+export const mockedUploadGroupOfBeneficiary = {
+  elabTimeStamp: new Date('2018-10-13T00:00:00.000Z'),
+  errorKey: 'errorKey',
+  errorRow: 0,
+  status: 'APPROVED',
+};
+
 export const getGroupOfBeneficiaryStatusAndDetails = (_id: string) =>
   groupsApiMocked.getGroupOfBeneficiaryStatusAndDetails(mockedInitiativeId);
 
