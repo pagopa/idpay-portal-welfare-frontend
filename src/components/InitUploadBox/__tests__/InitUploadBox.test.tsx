e5094c9e2602a3919165c9036cce574679761ea6
Oraldo Doci <oraldo.doci@nttdata.com>
2023-05-24 17:04:01 +0200

    removed useless comments

--- src/components/InitUploadBox/__tests__/InitUploadBox.test.tsx
+++ src/components/InitUploadBox/__tests__/InitUploadBox.test.tsx
@@ -5,7 +5,7 @@ import InitUploadBox from '../InitUploadBox';
 beforeEach(() => {
   jest.spyOn(console, 'warn').mockImplementation(() => {});
 });
-// hey
+
 describe('test suite for InitUploadBox', () => {
   test('render InitUploadBox', () => {
     renderWithContext(<InitUploadBox text="text" link="link" />);
