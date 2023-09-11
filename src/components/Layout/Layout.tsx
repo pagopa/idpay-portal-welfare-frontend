a251f9edb2bdea7d4fc76de5970a6c6ea2c1ff39
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-09-11 12:05:51 +0200

    working on sidebar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -57,7 +57,7 @@ const Layout = ({ children }: Props) => {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [location.pathname]);
 
   return (
     <Box
