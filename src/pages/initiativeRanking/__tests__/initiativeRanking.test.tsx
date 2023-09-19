5b5fce7d36a6d79868f90b8126b7d18da39375ed
Francesco Graziano <fgrazian@emeal.nttdata.com>
2023-09-19 17:40:38 +0200

    added unit tests

--- src/pages/initiativeRanking/__tests__/initiativeRanking.test.tsx
+++ src/pages/initiativeRanking/__tests__/initiativeRanking.test.tsx
@@ -6,6 +6,7 @@ import { SasToken } from '../../../api/generated/initiative/SasToken';
 import ROUTES from '../../../routes';
 import { renderWithContext } from '../../../utils/test-utils';
 import InitiativeRanking from '../initiativeRanking';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: (key: any) => key }),
@@ -50,14 +51,14 @@ describe('<InitiativeRanking />', () => {
         beneficiaryRankingStatus: 'ELIGIBLE_OK',
       },
     ],
-    pageNumber: 0,
-    pageSize: 0,
-    totalElements: 0,
-    totalPages: 0,
+    pageNumber: 1,
+    pageSize: 10,
+    totalElements: 1,
+    totalPages: 1,
     rankingStatus: 'READY',
     rankingPublishedTimestamp: new Date(),
     rankingGeneratedTimestamp: new Date(),
-    totalEligibleOk: 0,
+    totalEligibleOk: 1,
     totalEligibleKo: 0,
     totalOnboardingKo: 0,
     rankingFilePath: 'string',
@@ -107,6 +108,9 @@ describe('<InitiativeRanking />', () => {
 
     const filterBtn = screen.getByText('pages.initiativeRanking.form.filterBtn');
     fireEvent.click(filterBtn);
+
+    const resetBtn = screen.getByText('pages.initiativeRanking.form.resetFiltersBtn');
+    fireEvent.click(resetBtn);
   });
 
   test('test case of READY status from getInitiativeOnboardingRankingStatus', async () => {
@@ -114,34 +118,9 @@ describe('<InitiativeRanking />', () => {
       async (): Promise<PageOnboardingRankingsDTO> => new Promise((resolve) => resolve(mockedRes));
 
     renderWithContext(<InitiativeRanking />);
-
-    // test reset filter btn click
-    const resetFilterBtn = await screen.findByText('pages.initiativeRanking.form.resetFiltersBtn');
-    fireEvent.click(resetFilterBtn);
-
-    // const downloadRankingFileBtn = await screen.findByText(
-    //   'pages.initiativeRanking.publishModal.alertBtn'
-    // );
-    // fireEvent.click(downloadRankingFileBtn);
-
-    // publish btn
-    const publishBtn = screen.getByText(
-      'pages.initiativeRanking.rankingStatus.publishBtn'
-    ) as HTMLButtonElement;
-
-    fireEvent.click(publishBtn);
-
-    const publishModalTitle = await screen.findByText('pages.initiativeRanking.publishModal.title');
-    expect(publishModalTitle).toBeInTheDocument();
-
-    const publishModalBtn = await screen.findByTestId('publish-button-test');
-    fireEvent.click(publishModalBtn);
-
-    const closeModalBtn = await screen.findByTestId('cancel-button-test');
-    fireEvent.click(closeModalBtn);
   });
 
-  test('test case of getInitiativeOnboardingRankingStatus with no content', async () => {
+  test('test case of READY status from getInitiativeOnboardingRankingStatus', async () => {
     InitiativeApiMocked.getInitiativeOnboardingRankingStatusPaged =
       async (): Promise<PageOnboardingRankingsDTO> =>
         new Promise((resolve) => resolve(mockedResEmptyContent));
