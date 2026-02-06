3eceb14f693045bdcfa55d4105f08e33ca578e2b
abilello1 <alessio.bilello@emeal.nttdata.com>
2026-02-06 16:52:13 +0100

    minor refactor BreadcrumbsBox

--- src/pages/components/BreadcrumbsBox.tsx
+++ src/pages/components/BreadcrumbsBox.tsx
@@ -11,21 +11,33 @@ interface Props {
 
 const BreadcrumbsBox = ({ backUrl, backLabel, items }: Props) => {
   const history = useHistory();
+  const filteredItems = items.filter(Boolean);
+
   return (
-    <Box sx={{ display: 'grid', gridColumn: 'span 12' }}>
+    <Box sx={{
+      display: 'flex',
+      alignItems: 'center',
+      gap: 1,
+      gridColumn: 'span 12'
+    }}>
+      <ButtonNaked
+        component="button"
+        onClick={() => history.replace(backUrl)}
+        startIcon={<ArrowBackIcon />}
+        sx={{ color: 'primary.main', fontSize: '1rem' }}
+        weight="default"
+        data-testid="back-btn-test"
+      >
+        {backLabel}
+      </ButtonNaked>
+
       <Breadcrumbs aria-label="breadcrumb">
-        <ButtonNaked
-          component="button"
-          onClick={() => history.replace(backUrl)}
-          startIcon={<ArrowBackIcon />}
-          sx={{ color: 'primary.main', fontSize: '1rem', marginBottom: '3px' }}
-          weight="default"
-          data-testid="back-btn-test"
-        >
-          {backLabel}
-        </ButtonNaked>
-        {items.map((label, index) => (
-          <Typography color="text.primary" variant="body2" key={index}>
+        {filteredItems.map((label, index) => (
+          <Typography
+            color={index === filteredItems.length - 1 ? 'text.disabled' : 'text.primary'}
+            variant="body2"
+            key={index}
+          >
             {label}
           </Typography>
         ))}
