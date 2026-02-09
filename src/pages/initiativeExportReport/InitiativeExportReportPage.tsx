5f4d8318bea448ab545dc9eeefabc02ca2ecf897
Francesco Fico <Francesco.Fico@nttdata.com>
2026-02-09 11:34:53 +0100

    feat: [UPBE-592] Export Report Page merchant's filter (#175)

--- src/pages/initiativeExportReport/InitiativeExportReportPage.tsx
+++ src/pages/initiativeExportReport/InitiativeExportReportPage.tsx
@@ -1,6 +1,8 @@
 import { Box, Table, TableBody, TableCell, TableRow, Typography } from "@mui/material";
 import { TitleBox } from "@pagopa/selfcare-common-frontend";
 import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from "react";
+import { matchPath } from "react-router-dom";
 import { initiativePagesBreadcrumbsContainerStyle } from "../../helpers";
 import ROUTES from "../../routes";
 import BreadcrumbsBox from "../components/BreadcrumbsBox";
@@ -8,12 +10,42 @@ import { initiativeSelector } from "../../redux/slices/initiativeSlice";
 import { useAppSelector } from "../../redux/hooks";
 import { useInitiative } from "../../hooks/useInitiative";
 import ExportFiltersCard from "../../components/ExportFiltersCard/ExportFiltersCard";
+import { getMerchantList } from "../../services/merchantsService";
+import { MerchantItem } from "../initiativeRefundsMerchants/initiativeRefundsMerchants";
+import { useAlert } from "../../hooks/useAlert";
 
 
 const InitiativeExportReportPage = () => {
     const { t } = useTranslation();
     const initiativeSel = useAppSelector(initiativeSelector);
     useInitiative();
+    const [businessNameList, setBusinessNameList] = useState<Array<MerchantItem>>([]);
+    interface MatchParams {
+        id: string;
+    }
+
+    const { setAlert } = useAlert();
+
+    const match = matchPath(location.pathname, {
+        path: [ROUTES.INITIATIVE_EXPORT_REPORT],
+        exact: true,
+        strict: false,
+    });
+    const { id } = (match?.params as MatchParams) || {};
+
+    useEffect(() => {
+        getMerchantsList();
+    }, []);
+
+    const getMerchantsList = () => {
+        getMerchantList(id, 0).then((res) => {
+            if (res && res.content && res.content.length > 0) {
+                setBusinessNameList(res.content as Array<MerchantItem>);
+            }
+        }).catch(() => {
+            setAlert({ title: t('errors.title'), text: t('errors.getDataDescription'), isOpen: true, severity: 'error' });
+        });
+    };
 
     return (
         <Box sx={{ width: '100%', pt: 2, px: 2 }}>
@@ -39,6 +71,7 @@ const InitiativeExportReportPage = () => {
             <Box sx={{ display: 'flex', gap: 3, mt: 1, mb: 3, alignItems: 'center' }}>
                 <ExportFiltersCard
                     onGenerateReport={() => console.log('button genera')}
+                    businessList={businessNameList}
                 />
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3, mt: 1, mb: 3 }}>
