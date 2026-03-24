3ff23ecd557f9cfd54ac1212d34a5e6747befde5
abilello1 <alessio.bilello@emeal.nttdata.com>
2026-03-24 16:22:37 +0100

    refactor index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
+import '@pagopa/selfcare-common-frontend/lib/common-polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import '@pagopa/selfcare-common-frontend/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
-import { CONFIG } from '@pagopa/selfcare-common-frontend/config/env';
+import { CONFIG } from '@pagopa/selfcare-common-frontend/lib/config/env';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { MOCK_USER, testToken } from './utils/constants';
 import { ENV } from './utils/env';
-import './consentAndAnalyticsConfiguration.ts';
+import './consentAndAnalyticsConfiguration';
 import './locale';
 import ROUTES from './routes';
 import { initializeOneTrustLinkFix } from './utils/onetrust-utils';
@@ -27,13 +28,20 @@ CONFIG.URL_FE.LOGOUT = ENV.URL_FE.LOGOUT;
 CONFIG.URL_FE.ASSISTANCE = ENV.URL_FE.ASSISTANCE_PORTAL;
 // eslint-disable-next-line functional/immutable-data
 CONFIG.TEST.JWT = testToken;
-
 // eslint-disable-next-line functional/immutable-data
 CONFIG.HEADER.LINK.PRODUCTURL = ROUTES.HOME;
 
 initializeOneTrustLinkFix('/portale-enti');
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+if (rootElement === null) {
+  throw new Error('Root element with id "root" not found');
+}
+
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -43,8 +51,7 @@ ReactDOM.render(
         </ThemeProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
