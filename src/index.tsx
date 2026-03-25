19656139854d1f1affe49bc84efe42bffa9da466
abilello1 <alessio.bilello@emeal.nttdata.com>
2026-03-25 15:47:26 +0100

    init config before app import

--- src/index.tsx
+++ src/index.tsx
@@ -7,31 +7,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from '@pagopa/mui-italia';
-import { CONFIG } from '@pagopa/selfcare-common-frontend/lib/config/env';
+import './bootstrap';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
-import { MOCK_USER, testToken } from './utils/constants';
-import { ENV } from './utils/env';
 import './consentAndAnalyticsConfiguration';
 import './locale';
-import ROUTES from './routes';
-import { initializeOneTrustLinkFix } from './utils/onetrust-utils';
-
-// eslint-disable-next-line functional/immutable-data
-CONFIG.MOCKS.MOCK_USER = MOCK_USER;
-// eslint-disable-next-line functional/immutable-data
-CONFIG.URL_FE.LOGIN = ENV.URL_FE.LOGIN;
-// eslint-disable-next-line functional/immutable-data
-CONFIG.URL_FE.LOGOUT = ENV.URL_FE.LOGOUT;
-// eslint-disable-next-line functional/immutable-data
-CONFIG.URL_FE.ASSISTANCE = ENV.URL_FE.ASSISTANCE_PORTAL;
-// eslint-disable-next-line functional/immutable-data
-CONFIG.TEST.JWT = testToken;
-// eslint-disable-next-line functional/immutable-data
-CONFIG.HEADER.LINK.PRODUCTURL = ROUTES.HOME;
-
-initializeOneTrustLinkFix('/portale-enti');
 
 const rootElement = document.getElementById('root');
 
