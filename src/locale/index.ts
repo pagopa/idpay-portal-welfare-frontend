3a6763b66a812a46ac1d74697a140a54dd231bca
abilello1 <alessio.bilello@emeal.nttdata.com>
2026-03-25 16:23:41 +0100

    refactor i18n config

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,17 +1,5 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import it from './it.json';
-
-void i18n.use(initReactI18next).init({
-  resources: {
-    it: { translation: it },
-  },
-  lng: 'it',
-  fallbackLng: 'it',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+import i18n from './i18n';
+import './i18nSelfcare';
 
 export const t = (key: string, options?: Record<string, unknown>): string =>
   (i18n as any).t(key, options);
